4af9412f706f882cb36838188174bc1721ffed70
alan-stokes <alan.barry.stokes@gmail.com>
2014-06-13 15:42:35 +0100

    cleaned up the vertex comments and added the start of the edge object

--- pacman/graph/edge.py
+++ pacman/graph/edge.py
@@ -1,10 +1,16 @@
 __author__ = 'stokesa6'
 class Edge(object):
-    '''
+    """An edge object"""
 
-    '''
-
-    def __init__(self):
-        '''
-
-        '''
+    def __init__(self, pre_vertex, post_vertex, label):
+        """Create a new edge
+        :param label: The name of the edge
+        :type label: str
+        :param pre_vertex: the outgoing vertex
+        :type pre_vertex: pacman.graph.vertex.Vertex
+        :param post_vertex:  the incoming vertex
+        :type post_vertex: pacman.graph.vertex.Vertex
+        :return: an Edge
+        :rtype: pacman.graphg.edge.Edge
+        :raises None: Raises no known exceptions
+        """
--- pacman/graph/vertex.py
+++ pacman/graph/vertex.py
@@ -1,7 +1,9 @@
 class Vertex(object):
+    """ a vertex object """
+
     
     def __init__(self, label, n_atoms, constraints=None):
-        ''' Create a new vertex
+        """ Create a new vertex
         :param label: The name of the vertex
         :type label: str 
         :param n_atoms: The number of atoms that the vertex can be split into
@@ -11,30 +13,30 @@ class Vertex(object):
         :return: a Vertex object
         :rtype: pacman.graph.vertex.Vertex
         :raises None: Raises no known exceptions
-        '''
+        """
         pass
     
     @property
     def label(self):
-        ''' Get the label of the vertex
+        """ Get the label of the vertex
         :return: The name of the vertex
         :rtype: str
         :raises None: Raises no known exceptions
-        '''
+        """
         pass
     
     @property
     def n_atoms(self):
-        ''' Get the number of atoms in the vertex
+        """ Get the number of atoms in the vertex
         :return: The number of atoms in the vertex
         :rtype: int
         :raises None: Raises no known exceptions
-        '''
+        """
         pass
     
     @property
     def constraints(self):
-        ''' 
+        """ 
         
-        '''
+        """
         pass
