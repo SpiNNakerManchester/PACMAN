0401a142a69a21f076c19d12c2d7083027bee111
alan-stokes <alan.barry.stokes@gmail.com>
2014-06-13 15:07:11 +0100

    added the start of the graphg object

---
+++ pacman/graph/graph.py
@@ -0,0 +1,19 @@
+__author__ = 'stokesa6'
+
+class Graph(object):
+
+
+    def __init__(label, vertexes=None, edges=None):
+        ''' create a graph object
+        :param label: a identifier for the graph
+        :param vertexes: a collection of vertexes
+        :param edges: a collection of edges
+        :type label: str
+        :type vertexes: None or iterable object
+        :type edges: None or iterable object
+        :return: a new graph object
+        :rtype: pacman.graph.graph.Graph
+        :raises None: does not raise any known exceptions
+        '''
+
+
