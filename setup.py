4b32cbabc310a64ac44a742352919cfa4477aa1f
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-07-14 14:54:13 +0100

    Adding setup and requirements for docs

---
+++ setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="PACMAN",
+    version="0.1-SNAPSHOT",
+    description="Partition and Configuration Manager",
+    url="https://github.com/SpiNNakerManchester/PACMAN",
+    packages=['pacman',
+            'pacman.model',
+            'pacman.model.constraints',
+            'pacman.model.graph',
+            'pacman.model.placements',
+            'pacman.model.resources',
+            'pacman.model.routing_info',
+            'pacman.model.routing_tables',
+            'pacman.model.subgraph',
+            'pacman.operations',
+            'pacman.operations.partition_algorithms',
+            'pacman.operations.placer_algorithms',
+            'pacman.operations.router_algorithms',
+            'pacman.operations.routing_info_allocator_algorithms'],
+    install_requires=['six', 'enum34', 'SpiNNMachine']
+)
