4b22eda67219cae0036a85842f0375a8cb4ebca2
Petrut Antoniu Bogdan <petrutantoniu8@gmail.com>
2014-07-17 10:31:43 +0100

    Created test skeleton for subgraph

---
+++ unittests/test_subgraph_model.py
@@ -0,0 +1,12 @@
+import unittest
+from pacman.model.subgraph.subvertex import Subvertex
+from pacman.model.subgraph.subgraph import Subgraph
+from pacman.model.subgraph.subedge import Subedge
+
+class TestSubgraphModel(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
