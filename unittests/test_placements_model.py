7d83ae531d5f6698d8797aaa1087a878509eb3bc
Petrut Antoniu Bogdan <petrutantoniu8@gmail.com>
2014-07-21 14:40:19 +0100

    Created tests for the placements module. Detected bugs

--- unittests/test_placements_model.py
+++ unittests/test_placements_model.py
@@ -1,10 +1,79 @@
 import unittest
 from pacman.model.placements.placement import Placement
 from pacman.model.placements.placements import Placements
+from pacman.model.subgraph.subvertex import Subvertex
+from pacman.exceptions import PacmanAlreadyExistsException
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class TestPlacements(unittest.TestCase):
+    def test_create_new_placement(self):
+        subv = Subvertex(0,100)
+        pl = Placement(subv,0,0,1)
+        self.assertEqual(pl.x,0)
+        self.assertEqual(pl.y,0)
+        self.assertEqual(pl.p,1)
+        self.assertEqual(subv,pl.subvertex)
+
+    def test_create_new_placements_duplicate_subvertex(self):
+        with self.assertRaises(PacmanAlreadyExistsException):
+            subv = Subvertex(0,100)
+            pl = list()
+            for i in range(4):
+                pl.append(Placement(subv,0,0,i))
+
+            pls = Placements(pl)
+
+    def test_create_new_placements(self):
+        subv = Subvertex(0,100)
+        pl = Placement(subv,0,0,1)
+        pls = Placements([pl])
+
+    def test_create_new_empty_placements(self):
+        pls = Placements()
+        self.assertEqual(pls._placements,dict())
+        self.assertEqual(pls._subvertices,dict())
+
+    def test_get_placement_of_subvertex(self):
+        subv = list()
+        for i in range(5):
+            subv.append(Subvertex(i,2*i))
+
+        pl = list()
+        for i in range(4):
+            pl.append(Placement(subv[i],0,0,i))
+
+        pls = Placements(pl)
+        for i in range(4):
+            self.assertEqual(pls.get_placement_of_subvertex(subv[i]),pl[i])
+
+    def test_get_subvertex_on_processor(self):
+        subv = list()
+        for i in range(5):
+            subv.append(Subvertex(i,2*i))
+
+        pl = list()
+        for i in range(4):
+            pl.append(Placement(subv[i],0,0,i))
+
+        pls = Placements(pl)
+        for i in range(4):
+            self.assertEqual(pls.get_subvertex_on_processor(0,0,i),subv[i])
+
+        self.assertEqual(pls.get_placement_of_subvertex(subv[0]),pl[0])
+
+    def test_get_placements(self):
+        subv = list()
+        for i in range(5):
+            subv.append(Subvertex(i,2*i))
+
+        pl = list()
+        for i in range(4):
+            pl.append(Placement(subv[i],0,0,i))
+
+        pls = Placements(pl)
+        container = pls.placements
+        for i in range(4):
+            self.assertIn(pl[i],container)
 
 
 if __name__ == '__main__':
