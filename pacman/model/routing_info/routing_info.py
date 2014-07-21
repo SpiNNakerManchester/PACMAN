b20a65d3fbe030d67320518d27a0d4eba2ce797c
Petrut Antoniu Bogdan <petrutantoniu8@gmail.com>
2014-07-21 16:20:51 +0100

    routing info now throws exception if the key mask combo is the same with different pre_subvertex, stores subedge_info in a dictionary keyed on key mask combo, get_subedge_info_by_key returns single entry not an iterable

--- pacman/model/routing_info/routing_info.py
+++ pacman/model/routing_info/routing_info.py
@@ -19,10 +19,8 @@ class RoutingInfo(object):
         self._subedge_info_by_key = dict()
         self._subedge_info = list()
         if subedge_info_items is not None:
-            self._subedge_info = list(subedge_info_items)
-
-        for subedge_info_item in subedge_info_items:
-            self.add_subedge_info(subedge_info_item)
+            for subedge_info_item in subedge_info_items:
+                self.add_subedge_info(subedge_info_item)
 
     def add_subedge_info(self, subedge_info):
         """ Add a subedge information item
@@ -36,11 +34,14 @@ class RoutingInfo(object):
                     already an item with the same key once the mask is applied\
                     which does not have the same source subvertex
         """
-        if subedge_info.key in self._subedge_info_by_key.keys():
+        if subedge_info.key_mask_combo in self._subedge_info_by_key.keys()\
+                and subedge_info.subedge.pre_subvertex is not\
+                self._subedge_info_by_key[subedge_info.key_mask_combo].subedge.pre_subvertex:
             raise PacmanAlreadyExistsException("The key already exists in the routing information",
-                                               str(subedge_info.key))
-        else:
-            self._subedge_info_by_key[subedge_info.key] = subedge_info
+                                               str(subedge_info.key_mask_combo))
+
+        self._subedge_info_by_key[subedge_info.key_mask_combo] = subedge_info
+        self._subedge_info.append(subedge_info)
 
     @property
     def all_subedge_info(self):
@@ -61,15 +62,13 @@ class RoutingInfo(object):
         :type key: int
         :param mask: The routing mask
         :type mask: int
-        :return: an iterable of routing information associated with the\
+        :return: a routing information associated with the\
                     specified routing key or None if no such key exists
-        :rtype: iterable of\
-                    :py:class:`pacman.model.routing_info.subedge_routing_info.SubedgeRoutingInfo`
+        :rtype: :py:class:`pacman.model.routing_info.subedge_routing_info.SubedgeRoutingInfo`
         :raise None: does not raise any known exceptions
         """
-        key_and_mask = key & mask
-        list_of_accepting_key_mask_combinations = list()
-        if key_and_mask in self._subedge_info_by_key.keys():
-            list_of_accepting_key_mask_combinations.append(self._subedge_info_by_key[key_and_mask])
-        return list_of_accepting_key_mask_combinations
+        key_mask_combo = key & mask
+        if key_mask_combo in self._subedge_info_by_key.keys():
+            return self._subedge_info_by_key[key_mask_combo]
+        return None
 
