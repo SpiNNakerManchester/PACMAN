50b2898cd177d14f9ce1744d01624a4cecf2920d
alan-stokes <alan.barry.stokes@gmail.com>
2014-08-01 17:06:43 +0100

    added the start of router support

--- pacman/operations/router.py
+++ pacman/operations/router.py
@@ -1,4 +1,5 @@
 import logging
+from pacman.utilities import reports
 
 logger = logging.getLogger(__name__)
 
@@ -7,7 +8,9 @@ class Router:
     """ Used to find routes through a machine
     """
 
-    def __init__(self, router_algorithm=None):
+    def __init__(self, machine, report_states, placements, subgraph,
+                 routing_infos, report_folder=None, hostname=None,
+                 router_algorithm=None):
         """
         :param router_algorithm: The router algorithm.  If not specified, a\
                     default algorithm will be used
@@ -16,7 +19,20 @@ class Router:
         :raise pacman.exceptions.PacmanInvalidParameterException: If\
                     router_algorithm is not valid
         """
-        pass
+        self._report_folder = report_folder
+        self.report_states = report_states
+        self._hostname = hostname
+        self._machine = machine
+        self._subgraph = subgraph
+        self._routing_infos = routing_infos
+        self._placements = placements
+        self._router_algorithm = router_algorithm
+
+        #set up a default placer algorithum if none are specified
+        if self._router_algorithm is None:
+            self._router_algorithm =
+        else:
+            self._router_algorithm =
 
     def run(self, routing_info_allocation, placements, machine):
         """ Execute the router algorithm, finding the routes through the\
@@ -35,4 +51,16 @@ class Router:
         :raise pacman.exceptions.PacmanRoutingException: If something\
                    goes wrong with the routing
         """
-        pass
+        placements = \
+            self._router_algorithm.route(subgraph, graph_to_subgraph_mapper)
+
+        #execute reports if needed
+        if (self.report_states is not None and
+                self.report_states.router_reports):
+            reports.router_report(
+                graph=self._graph, hostname=self._hostname,
+                graph_to_subgraph_mapper=graph_to_subgraph_mapper,
+                machine=self._machine, placements=placements,
+                report_folder=self._report_folder)
+
+        return placements
