c9d9c86a3131ce3e923503f6f2f86060b13c6ea2
alan-stokes <alan.barry.stokes@gmail.com>
2014-08-05 14:26:56 +0100

    added a virtual chip constraint and added a note in the cfg

---
+++ pacman/model/constraints/vertex_requires_virtual_chip_in_machine_constraint.py
@@ -0,0 +1,65 @@
+from pacman.model.constraints.abstract_utility_constraint import \
+    AbstractUtilityConstraint
+
+
+class VertexRequiresVirtualChipInMachineConstraint(AbstractUtilityConstraint):
+    """ A constraint which indicates that a vertex has a requirement for some
+    multicast packets to be trnasmitted at given times
+    itself to a multicast source
+    """
+
+    def __init__(self, virtual_chip_coords, connected_to_chip_coords,
+                 connected_chip_link_id):
+        """
+
+        :param virtual_chip_coords: The virtual coords of the virtual chip
+        :param connected_to_chip_coords: the chip coords to which the virtual \
+        chip connects to
+        :param connected_chip_link_id: the link id that the virtual chip \
+        connects to the real chip
+        :type virtual_chip_coords: tupe with x and y
+        :type connected_to_chip_coords:  tupe with x and y
+        :type connected_chip_link_id: int
+        :raise None: does not raise any known exceptions
+        """
+        AbstractUtilityConstraint.__init__(
+            self, "Vertex Requires a virtual chip in the machine with coords "
+                  "{}:{} connected to the real chip at coords {}:{} on link {}"
+                  .format(virtual_chip_coords['x'], virtual_chip_coords['y'],
+                          connected_to_chip_coords['x'],
+                          connected_to_chip_coords['y'],
+                          connected_chip_link_id))
+        self._virtual_chip_coords = virtual_chip_coords
+        self._connected_to_chip_coords = connected_to_chip_coords
+        self._connected_chip_link_id = connected_chip_link_id
+
+    @property
+    def virtual_chip_coords(self):
+        """ the virtual chip coords
+
+        :return: the virtual chip coords
+        :rtype: tuple
+        :raise None: does not raise any known exceptions
+        """
+        return self._virtual_chip_coords
+
+    @property
+    def connected_to_chip_coords(self):
+        """ the chip to which the virutal chip connects to
+
+        :return: the chip to which the virutal chip connects to
+        :rtype: tuple
+        :raise None: does not raise any known exceptions
+        """
+        return self._connected_to_chip_coords
+
+
+    @property
+    def connected_to_chip_coords(self):
+        """ the link on the chip to which the virutal chip connects to
+
+        :return: the link on the chip to which the virutal chip connects to
+        :rtype: tuple
+        :raise None: does not raise any known exceptions
+        """
+        return self._connected_chip_link_id
