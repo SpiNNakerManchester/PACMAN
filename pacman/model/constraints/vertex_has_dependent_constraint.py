ee312b81b817a5fa61317c66df4a3b38194829fe
Petrut Antoniu Bogdan <petrutantoniu8@gmail.com>
2014-08-14 16:22:38 +0100

    Modified string formatting + fixed typos in vertex_has_dependent_constraint Modified vertex_requires_virtual_chip_in_machine_constraint calls so that they call x and y properly from a dictionary, instead of a tuple

--- pacman/model/constraints/vertex_has_dependent_constraint.py
+++ pacman/model/constraints/vertex_has_dependent_constraint.py
@@ -4,19 +4,20 @@ from pacman.model.constraints.abstract_utility_constraint import \
 
 class VertexHasDependentConstraint(AbstractUtilityConstraint):
     """ A constraint which indicates that a vertex A must have a edge to another
-    vertex B, where B only exists becuase A uses it.
+    vertex B, where B only exists because A uses it.
     """
 
     def __init__(self, vertex):
         """
 
         :param vertex: The vertex to which the constraint refers
-        :type vertex: :py:class:`pacman.model.graph.vertex.AbstractConstrainedVertex`
+        :type vertex:
+        :py:class:`pacman.model.graph.vertex.AbstractConstrainedVertex`
         :raise None: does not raise any known exceptions
         """
         AbstractUtilityConstraint.__init__(
-            self, "vertex {} requires vertex {} to oeprate correctly"
-                  "{}".format(vertex))
+            self, "vertex {} requires another vertex to operate correctly"
+            .format(vertex))
         self._vertex = vertex
 
     def is_utility_constraint(self):
@@ -27,7 +28,8 @@ class VertexHasDependentConstraint(AbstractUtilityConstraint):
         """ The vertex to link with
 
         :return: the vertex
-        :rtype: :py:class:`pacman.model.graph.vertex.AbstractConstrainedVertex`
+        :rtype:
+        :py:class:`pacman.model.graph.vertex.AbstractConstrainedVertex`
         :raise None: does not raise any known exceptions
         """
         return self._vertex
