58a6194a6d3a0f7d72790f1edbb3be86d800e54d
Andrew Mundy <andrew.mundy@ieee.org>
2014-11-11 16:24:04 +0000

    Turn vertex slices into Python slices.
    Converts the `Slice` object so that it subclasses from `namedtuple`.  This
    allows precomputation of `n_atoms` and the creation of a Python `slice`.
    
    The intended use of the additional slice object is something like this:
    
    ```python
    def do_something_with_slice(vertex_slice):
        # ...
        prepared_data[vertex_slice.as_slice]
        # ...
    ```
    
    Allowing the vertex slice to be used directly to slice a Python array.  Saves
    doing this:
    
    ```python
    def do_something_with_slice(vertex_slice):
        # ...
        prepared_data[vertex_slice.lo_atom:vertex_slice.hi_atom + 1]
        # ...
    ```

--- pacman/model/graph_mapper/slice.py
+++ pacman/model/graph_mapper/slice.py
@@ -1,17 +1,38 @@
-class Slice(object):
+import collections
 
-    def __init__(self, lo_atom, hi_atom):
-        self._lo_atom = lo_atom
-        self._hi_atom = hi_atom
+from pacman.exceptions import PacmanValueError
 
-    @property
-    def lo_atom(self):
-        return self._lo_atom
 
-    @property
-    def hi_atom(self):
-        return self._hi_atom
+class Slice(collections.namedtuple('Slice', 
+                                   'lo_atom hi_atom n_atoms as_slice')):
+    """Represents a slice of a vertex.
 
-    @property
-    def n_atoms(self):
-        return (self._hi_atom - self.lo_atom) + 1
+    :attr int lo_atom: The lowest atom represented in the slice.
+    :attr int hi_atom: The highest atom represented in the slice.
+    :attr int n_atoms: The number of atoms represented by the slice.
+    :attr as_slice: This slice represented as a :py:func:`slice` object (for
+                    use in indexing lists, arrays, etc.)
+    """
+    def __new__(cls, lo_atom, hi_atom):
+        """Create a new Slice object.
+
+        :param int lo_atom: Index of the lowest atom to represent.
+        :param int hi_atom: Index of the highest atom to represent.
+        :raises PacmanValueError: If the bounds of the slice are invalid.
+        """
+        if lo_atom < 0:
+            raise PacmanValueError('lo_atom < 0')
+        if hi_atom < lo_atom:
+            raise PacmanValueError(
+                'hi_atom {:d} < lo_atom {:d}'.format(hi_atom,lo_atom))
+
+        # Number of atoms represented by this slice
+        n_atoms = hi_atom - lo_atom + 1
+
+        # Slice for accessing arrays of values
+        as_slice = slice(lo_atom, hi_atom + 1)
+
+        # Create the Slice object as a `namedtuple` with these pre-computed
+        # values filled in.
+        return super(cls, Slice).__new__(cls, lo_atom, hi_atom, n_atoms,
+                                         as_slice)
