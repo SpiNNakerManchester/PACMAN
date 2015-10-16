fa0dc9a40e5bc7e59e79894b02734044a5742144
sergiodavies <daviess@cs.man.ac.uk>
2015-10-16 15:37:36 +0100

    Ongoing implementation of buffering out. Currently missing flushing data to host and passing iptag number from host to app with the handling of this value in the app To be reviewed - region IDs in spinnfrontendcommon need to go away, as these are only for neural application

---
+++ pacman/model/partitionable_graph/receive_buffers_to_host_partitionable_vertex.py
@@ -0,0 +1,46 @@
+from abc import abstractmethod, ABCMeta
+from six import add_metaclass
+from pacman.model.constraints.placer_constraints.\
+    placer_radial_placement_from_chip_constraint import \
+    PlacerRadialPlacementFromChipConstraint
+from pacman.model.constraints.tag_allocator_constraints.\
+    tag_allocator_require_iptag_constraint import \
+    TagAllocatorRequireIptagConstraint
+
+
+@add_metaclass(ABCMeta)
+class ReceiveBuffersToHostPartitionableVertex(object):
+    def __init__(self, buffering_output=False):
+        self._buffering_output = buffering_output
+
+    @property
+    def buffering_output(self):
+        return self._buffering_output
+
+    def set_buffering_output(self):
+        if not self._buffering_output:
+            self._buffering_output = True
+
+            # here I need to add the code to associate a tag to the vertex
+            notification_ip_address = None
+            notification_port = None
+            board_address = None
+            notification_tag = None
+            notification_strip_sdp = False
+
+            self.add_constraint(
+                TagAllocatorRequireIptagConstraint(
+                    notification_ip_address, notification_port,
+                    notification_strip_sdp, board_address, notification_tag))
+
+            # add placement constraint
+            placement_constraint = PlacerRadialPlacementFromChipConstraint(0, 0)
+            self.add_constraint(placement_constraint)
+
+    @abstractmethod
+    def add_constraint(self, constraint):
+        pass
+
+    @staticmethod
+    def is_buffering_recordable():
+        return True
