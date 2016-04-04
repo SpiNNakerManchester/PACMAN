5b99ed8c9086d56b1203803545b73cf8c9c4c5b3
alan-stokes <alan.barry.stokes@gmail.com>
2016-04-04 15:17:10 +0100

    fixed setup.py for release.

--- setup.py
+++ setup.py
@@ -11,13 +11,11 @@ setup(
     license="GNU GPLv3.0",
     packages=['pacman',
               'pacman.model',
+              'pacman.operations',
+              'pacman.utilities',
+
               'pacman.model.abstract_classes',
               'pacman.model.constraints',
-              'pacman.model.constraints.abstract_constraints',
-              'pacman.model.constraints.key_allocator_constraints',
-              'pacman.model.constraints.partitioner_constraints',
-              'pacman.model.constraints.placer_constraints',
-              'pacman.model.constraints.tag_allocator_constraints',
               'pacman.model.graph_mapper',
               'pacman.model.partitionable_graph',
               'pacman.model.partitioned_graph',
@@ -27,7 +25,13 @@ setup(
               'pacman.model.routing_table_by_partition',
               'pacman.model.routing_tables',
               'pacman.model.tags',
-              'pacman.operations',
+
+              'pacman.model.constraints.abstract_constraints',
+              'pacman.model.constraints.key_allocator_constraints',
+              'pacman.model.constraints.partitioner_constraints',
+              'pacman.model.constraints.placer_constraints',
+              'pacman.model.constraints.tag_allocator_constraints',
+
               'pacman.operations.algorithm_reports',
               'pacman.operations.chip_id_allocator_algorithms',
               'pacman.operations.multi_cast_router_check_functionality',
@@ -36,13 +40,16 @@ setup(
               'pacman.operations.rig_algorithms',
               'pacman.operations.router_algorithms',
               'pacman.operations.router_compressors',
-              'pacman.operations.router_compressors.mundys_router_compressor',
               'pacman.operations.routing_info_allocator_algorithms',
-              'pacman.operations.routing_info_allocator_algorjthms.field_based_routing_allocator',
-              'pacman.operations.routing_info_allocator_algorithms.malloc_based_routing_allocator',
-              'pacmna.operations.routing_table_generators',
+              'pacman.operations.routing_table_generators',
               'pacman.operations.tag_allocator_algorithms',
-              'pacman.utilities',
+
+
+              'pacman.operations.router_compressors.mundys_router_compressor',
+
+              'pacman.operations.routing_info_allocator_algorithms.field_based_routing_allocator',
+              'pacman.operations.routing_info_allocator_algorithms.malloc_based_routing_allocator',
+
               'pacman.utilities.algorithm_utilities',
               'pacman.utilities.file_format_converters',
               'pacman.utilities.file_format_schemas',
