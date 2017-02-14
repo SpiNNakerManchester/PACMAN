f4b5ea4eacdbd1b9e9ae68abf411add1946c42c5
Christian <brenninc@cs.man.ac.uk>
2017-02-14 16:07:03 +0000

    copywrite year updated

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PACMAN'
-copyright = u'2014'
+copyright = u'2014-2017'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
