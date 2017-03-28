f8accf947fdec88991811d07480f9cd60c2f1fc7
Christian <brenninc@cs.man.ac.uk>
2017-03-28 11:28:08 +0100

    changed setup.py to autogeneration of packages

--- setup.py
+++ setup.py
@@ -3,66 +3,37 @@ try:
 except ImportError:
     from distutils.core import setup
 
+from collections import defaultdict
+import os
+
+# Build a list of all project modules, as well as supplementary files
+main_package = "pacman"
+data_extensions = {".aplx", ".xml"}
+main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
+start = len(main_package_dir)
+packages = []
+package_data = defaultdict(list)
+for dirname, dirnames, filenames in os.walk(main_package_dir):
+    if '__init__.py' in filenames:
+        package = "{}{}".format(
+            main_package, dirname[start:].replace(os.sep, '.'))
+        packages.append(package)
+    for filename in filenames:
+        _, ext = os.path.splitext(filename)
+        if ext in data_extensions:
+            package = "{}{}".format(
+                main_package, dirname[start:].replace(os.sep, '.'))
+            package_data[package].append("*.{}".format(ext))
+            break
+
 setup(
     name="SpiNNaker_PACMAN",
     version="3.0.1",
     description="Partition and Configuration Manager",
     url="https://github.com/SpiNNakerManchester/PACMAN",
     license="GNU GPLv3.0",
-    packages=['pacman',
-
-              'pacman.executor',
-              'pacman.executor.algorithm_classes',
-              'pacman.executor.algorithm_decorators',
-
-              'pacman.model',
-              'pacman.model.abstract_classes',
-              'pacman.model.abstract_classes.impl',
-              'pacman.model.constraints',
-              'pacman.model.constraints.key_allocator_constraints',
-              'pacman.model.constraints.partitioner_constraints',
-              'pacman.model.constraints.placer_constraints',
-              'pacman.model.constraints.router_constraints',
-              'pacman.model.decorators',
-              'pacman.model.graphs',
-              'pacman.model.graphs.application',
-              'pacman.model.graphs.application.impl',
-              'pacman.model.graphs.common',
-              'pacman.model.graphs.impl',
-              'pacman.model.graphs.machine',
-              'pacman.model.graphs.machine.impl',
-              'pacman.model.placements',
-              'pacman.model.resources',
-              'pacman.model.routing_info',
-              'pacman.model.routing_table_by_partition',
-              'pacman.model.routing_tables',
-              'pacman.model.tags',
-
-              'pacman.operations',
-              'pacman.operations.algorithm_reports',
-              'pacman.operations.chip_id_allocator_algorithms',
-              'pacman.operations.multi_cast_router_check_functionality',
-              'pacman.operations.partition_algorithms',
-              'pacman.operations.placer_algorithms',
-              'pacman.operations.rig_algorithms',
-              'pacman.operations.router_algorithms',
-              'pacman.operations.router_compressors',
-              'pacman.operations.router_compressors.mundys_router_compressor',
-              'pacman.operations.routing_info_allocator_algorithms',
-              'pacman.operations.routing_info_allocator_algorithms.field_based_routing_allocator',
-              'pacman.operations.routing_info_allocator_algorithms.malloc_based_routing_allocator',
-              'pacman.operations.routing_table_generators',
-              'pacman.operations.tag_allocator_algorithms',
-
-              'pacman.utilities',
-              'pacman.utilities.algorithm_utilities',
-              'pacman.utilities.file_format_converters',
-              'pacman.utilities.file_format_schemas',
-              'pacman.utilities.utility_objs'],
-    package_data={'pacman.operations.algorithm_reports': ['*.xml'],
-                  'pacman.operations': ['*.xml', '*.xsd'],
-                  'pacman.utilities.file_format_converters': ['*.xml'],
-                  'pacman.utilities.file_format_schemas': ['*.json']},
+    packages=packages,
+    package_data=package_data,
     install_requires=[
         'six', 'enum34', 'numpy', 'lxml', 'jsonschema',
         'rig >= 2.0.0, < 3.0.0', 'SpiNNMachine >= 3.0.0, < 4.0.0']
