f70a5312bef6df6e0a4e9f5fd2dae37385d9363c
sara <summerton.sara@gmail.com>
2017-07-17 11:56:18 +0100

    The bare outlines of RIG conversions

--- pacman/operations/rigged_algorithms/rig_sequential_placer.py
+++ pacman/operations/rigged_algorithms/rig_sequential_placer.py
@@ -6,4 +6,9 @@ from six import next
 
 #from rig.place_and_route.constraints import \
 #    LocationConstraint, ReserveResourceConstraint
+#ignoring ReserveResourceConstraint - unnecessary functionality
 from pacman.model.placements import Placement
+
+#from rig.place_and_route.exceptions import \
+#    InvalidConstraintError, InsufficientResourceError
+from pacman.utilities.utility_objs import ResourceTracker
