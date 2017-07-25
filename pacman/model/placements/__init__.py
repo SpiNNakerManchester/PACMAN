e8f8b849dead1f9d7d937d99db1fd02ba17267a3
sara <summerton.sara@gmail.com>
2017-07-25 11:07:35 +0100

    ok!

--- pacman/model/placements/__init__.py
+++ pacman/model/placements/__init__.py
@@ -1,5 +1,4 @@
 from .placement import Placement
 from .placements import Placements
-# from .placements_copy import PlacementsCopy
 
-__all__ = ["Placement", "Placements"]  # "PlacementCopy"
+__all__ = ["Placement", "Placements"]
