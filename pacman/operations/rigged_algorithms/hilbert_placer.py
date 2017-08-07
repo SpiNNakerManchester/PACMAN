5c1a32a68671e749cbe96b206c14e97c07da16e6
sara <summerton.sara@gmail.com>
2017-08-07 16:07:35 +0100

    Cleaned up docstring for _check_constraints

--- pacman/operations/rigged_algorithms/hilbert_placer.py
+++ pacman/operations/rigged_algorithms/hilbert_placer.py
@@ -71,11 +71,12 @@ class HilbertPlacer(object):
             self, vertices, additional_placement_constraints=None):
         """ Ensure that the algorithm conforms to any required constraints.
         :param vertices: The vertices for which to check the constraints
-        :type vertices: object
+        :type vertices: dictionary object of vertices and associated constraints
         :param additional_placement_constraints:\
             Additional placement constraints supported by the algorithm doing\
             this check
-        :type additional_placement_constraints: set
+        :type additional_placement_constraints: set of :py:class:\
+            `pacman.model.constraints.placer_constraints.abstract_placer_constraint`
         """
 
         placement_constraints = {SameChipAsConstraint}
