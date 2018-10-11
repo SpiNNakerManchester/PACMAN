bb7c64057659d453866636eefda80b649a0919cb
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-10-11 17:01:18 +0100

    Schema

--- pacman/utilities/file_format_converters/convert_to_java_machine.py
+++ pacman/utilities/file_format_converters/convert_to_java_machine.py
@@ -24,27 +24,43 @@ class ConvertToJavaMachine(object):
             "Converting to JSON machine")
 
         for chip in machine.chips:
-            monitors = chip.n_processors - chip.n_user_processors
-            router_entries = chip.router.n_available_multicast_entries
-            router_clock_speed = chip.router.clock_speed
-            sdram = chip.sdram.size
-            tags = len(chip.tag_ids)
-            virtual = chip.virtual
-            break
+            if (chip.ip_address is None):
+                s_monitors = chip.n_processors - chip.n_user_processors
+                s_router_entries = chip.router.n_available_multicast_entries
+                s_router_clock_speed = chip.router.clock_speed
+                s_sdram = chip.sdram.size
+                s_virtual = chip.virtual
+                s_tags = chip._tag_ids
+                break
+
+        chip = machine.boot_chip
+        e_monitors = chip.n_processors - chip.n_user_processors
+        e_router_entries = chip.router.n_available_multicast_entries
+        e_router_clock_speed = chip.router.clock_speed
+        e_sdram = chip.sdram.size
+        e_virtual = chip.virtual
+        e_tags = chip._tag_ids
 
         # write basic stuff
         json_obj = {
             "width": machine.max_chip_x + 1,
             "height": machine.max_chip_y + 1,
-            "chipResources": {
-                "monitors": monitors,
-                "routerEntries": router_entries,
-                "routerClockSpeed": router_clock_speed,
-                "sdram": sdram,
-                "tags": tags,
-                "virtual": virtual},
+            "ethernetResources": {
+                "monitors": e_monitors,
+                "routerEntries": e_router_entries,
+                "routerClockSpeed": e_router_clock_speed,
+                "tags": list(e_tags),
+                "sdram": e_sdram,
+                "virtual": e_virtual},
+            "standardResources": {
+                "monitors": s_monitors,
+                "routerEntries": s_router_entries,
+                "routerClockSpeed": s_router_clock_speed,
+                "tags": list(s_tags),
+                "sdram": s_sdram,
+                "virtual": s_virtual},
             "chips": [],
-            "root": (machine.boot_x, machine.boot_y)
+            "root": list((machine.boot_x, machine.boot_y))
         }
 
         # handle chips
@@ -59,29 +75,48 @@ class ConvertToJavaMachine(object):
                     dead_links.append(link_id)
             if len(dead_links) > 0:
                 details["deadLinks"] = dead_links
-            if chip.ip_address is not None:
-                details['ipAddress'] = chip.ip_address
 
             exceptions = dict()
-            if (chip.n_processors - chip.n_user_processors) != monitors:
-                exceptions["monitors"] = chip.n_processors - chip.n_user_processors
-            if (chip.router.n_available_multicast_entries != router_entries):
-                exceptions["routerEntries"] = \
-                    chip.router.n_available_multicast_entries
-            if (chip.router.clock_speed != router_clock_speed):
-                exceptions["routerClockSpeed"] = \
-                    chip.router.n_available_multicast_entries
-            if chip.sdram.size != sdram:
-                exceptions["sdram"] = chip.sdram.size
-            if len(chip.tag_ids) != tags:
-                exceptions["tags"] = len(chip.tag_ids)
-            if chip.virtual != virtual:
-                exceptions["virtual"] = chip.virtual
+            if chip.ip_address is not None:
+                details['ipAddress'] = chip.ip_address
+                if (chip.n_processors - chip.n_user_processors) != e_monitors:
+                    exceptions["monitors"] = \
+                        chip.n_processors - chip.n_user_processors
+                if (chip.router.n_available_multicast_entries !=
+                        e_router_entries):
+                    exceptions["routerEntries"] = \
+                        chip.router.n_available_multicast_entries
+                if (chip.router.clock_speed != e_router_clock_speed):
+                    exceptions["routerClockSpeed"] = \
+                        chip.router.n_available_multicast_entries
+                if chip.sdram.size != e_sdram:
+                    exceptions["sdram"] = chip.sdram.size
+                if chip.virtual != e_virtual:
+                    exceptions["virtual"] = chip.virtual
+                if chip.tag_ids != e_tags:
+                    details["tags"] = list(chip.tag_ids)
+            else:
+                if (chip.n_processors - chip.n_user_processors) != s_monitors:
+                    exceptions["monitors"] = \
+                        chip.n_processors - chip.n_user_processors
+                if (chip.router.n_available_multicast_entries !=
+                        s_router_entries):
+                    exceptions["routerEntries"] = \
+                        chip.router.n_available_multicast_entries
+                if (chip.router.clock_speed != s_router_clock_speed):
+                    exceptions["routerClockSpeed"] = \
+                        chip.router.n_available_multicast_entries
+                if chip.sdram.size != s_sdram:
+                    exceptions["sdram"] = chip.sdram.size
+                if chip.virtual != s_virtual:
+                    exceptions["virtual"] = chip.virtual
+                if chip.tag_ids != s_tags:
+                    details["tags"] = list(chip.tag_ids)
+
             if len(exceptions) > 0:
                 json_obj["chips"].append([chip.x, chip.y, details, exceptions])
             else:
                 json_obj["chips"].append([chip.x, chip.y, details])
-
         progress.update()
 
         # dump to json file
@@ -91,7 +126,7 @@ class ConvertToJavaMachine(object):
         progress.update()
 
         # validate the schema
-        #file_format_schemas.validate(json_obj, "jmachine.json")
+        file_format_schemas.validate(json_obj, "jmachine.json")
 
         # update and complete progress bar
         progress.end()
