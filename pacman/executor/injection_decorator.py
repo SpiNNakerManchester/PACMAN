e98d5969f16325e7a17f9909a98050e6bdbd7e6e
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-02-12 13:16:51 +0000

    getargspec is depricated

--- pacman/executor/injection_decorator.py
+++ pacman/executor/injection_decorator.py
@@ -2,7 +2,11 @@ try:
     from collections.abc import defaultdict
 except ImportError:
     from collections import defaultdict
-import inspect
+try:
+    from inspect import getfullargspec
+except ImportError:
+    # Python 2.7 hack
+    from inspect import getargspec as getfullargspec
 from functools import wraps
 from six import iteritems, itervalues
 
@@ -93,7 +97,7 @@ def inject_items(types):
     """
     def wrap(wrapped_method):
         exn_arg = None
-        method_args = inspect.getargspec(wrapped_method)
+        method_args = getfullargspec(wrapped_method)
         for type_arg in types:
             if type_arg not in method_args.args:
                 # Can't raise exception until run time
