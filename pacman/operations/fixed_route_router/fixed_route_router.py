f43e79f2081bc2c06cf2d972624ba21c45261ce0
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2019-05-31 14:28:41 +0100

    Using the create_one_board_machine function from spinn_machine

--- pacman/operations/fixed_route_router/fixed_route_router.py
+++ pacman/operations/fixed_route_router/fixed_route_router.py
@@ -1,7 +1,6 @@
 from six import itervalues
 from spinn_utilities.progress_bar import ProgressBar
-from spinn_machine import (
-    Router, FixedRouteEntry, Machine, virtual_machine)
+from spinn_machine import FixedRouteEntry, Machine, machine_factory
 from pacman.model.graphs.machine import (
     MachineVertex, MachineGraph, MachineEdge)
 from pacman.model.placements import Placements, Placement
@@ -11,7 +10,7 @@ from pacman.exceptions import (
 
 
 class FixedRouteRouter(object):
-    """ Fixed route router that makes a mirror path on every board based off\
+    r""" Fixed route router that makes a mirror path on every board based off\
     the diagram below. It assumed there's a core on the Ethernet-connected\
     chip that is of the destination class. ::
 
@@ -126,58 +125,31 @@ class FixedRouteRouter(object):
         graph = MachineGraph(label="routing graph")
         fake_placements = Placements()
 
+        # Create a fake machine consisting of only the one board that
+        # the routes should go over
+        fake_machine = machine_factory.create_one_board_machine(
+            board_version, machine, ethernet_connected_chip)
+
         # build fake setup for the routing
         eth_x = ethernet_connected_chip.x
         eth_y = ethernet_connected_chip.y
-        down_links = set()
-        for (chip_x, chip_y) in machine.get_chips_on_board(
-                ethernet_connected_chip):
+        for chip in machine.get_chips_by_ethernet(eth_x, eth_y):
             vertex = RoutingMachineVertex()
             graph.add_vertex(vertex)
-            rel_x = chip_x - eth_x
-            if rel_x < 0:
-                rel_x += machine.max_chip_x + 1
-            rel_y = chip_y - eth_y
-            if rel_y < 0:
-                rel_y += machine.max_chip_y + 1
 
             free_processor = 0
             while ((free_processor < machine.MAX_CORES_PER_CHIP) and
                    fake_placements.is_processor_occupied(
-                       self.FAKE_ETHERNET_CHIP_X,
+                       x=self.FAKE_ETHERNET_CHIP_X,
                        y=self.FAKE_ETHERNET_CHIP_Y,
                        p=free_processor)):
                 free_processor += 1
 
+            rel_x, rel_y = machine.get_local_xy(machine.get_chip_at(*chip))
             fake_placements.add_placement(Placement(
                 x=rel_x, y=rel_y, p=free_processor, vertex=vertex))
-            down_links.update({
-                (rel_x, rel_y, link) for link in range(
-                    Router.MAX_LINKS_PER_ROUTER)
-                if not machine.is_link_at(chip_x, chip_y, link)})
-
-        # Create a fake machine consisting of only the one board that
-        # the routes should go over
-        fake_machine = machine
-        if (board_version in machine.BOARD_VERSION_FOR_48_CHIPS and
-                (machine.max_chip_x > machine.MAX_CHIP_X_ID_ON_ONE_BOARD or
-                 machine.max_chip_y > machine.MAX_CHIP_Y_ID_ON_ONE_BOARD)):
-            down_chips = {
-                (x, y) for x, y in zip(
-                    range(machine.SIZE_X_OF_ONE_BOARD),
-                    range(machine.SIZE_Y_OF_ONE_BOARD))
-                if not machine.is_chip_at(
-                    (x + eth_x) % (machine.max_chip_x + 1),
-                    (y + eth_y) % (machine.max_chip_y + 1))}
-
-            # build a fake machine which is just one board but with the missing
-            # bits of the real board
-            fake_machine = virtual_machine(
-                machine.SIZE_X_OF_ONE_BOARD, machine.SIZE_Y_OF_ONE_BOARD,
-                False, down_chips=down_chips, down_links=down_links)
 
         # build destination
-        verts = graph.vertices
         vertex_dest = RoutingMachineVertex()
         graph.add_vertex(vertex_dest)
         destination_processor = self._locate_destination(
@@ -190,16 +162,14 @@ class FixedRouteRouter(object):
             p=destination_processor, vertex=vertex_dest))
 
         # deal with edges
-        for vertex in verts:
+        for vertex in graph.vertices:
             graph.add_edge(
                 MachineEdge(pre_vertex=vertex, post_vertex=vertex_dest),
                 self.FAKE_ROUTING_PARTITION)
 
         # route as if using multicast
-        router = BasicDijkstraRouting()
-        routing_tables_by_partition = router(
-            placements=fake_placements, machine=fake_machine,
-            machine_graph=graph, use_progress_bar=False)
+        routing_tables_by_partition = self.__dijkstra_route(
+            fake_machine, graph, fake_placements)
 
         # convert to fixed route entries
         for (chip_x, chip_y) in routing_tables_by_partition.get_routers():
@@ -210,13 +180,18 @@ class FixedRouteRouter(object):
             fixed_route_entry = FixedRouteEntry(
                 link_ids=mc_entry.link_ids,
                 processor_ids=mc_entry.processor_ids)
-            x = (chip_x + eth_x) % (machine.max_chip_x + 1)
-            y = (chip_y + eth_y) % (machine.max_chip_y + 1)
-            key = (x, y)
+            key = machine.get_global_xy(chip_x, chip_y, eth_x, eth_y)
             if key in fixed_route_tables:
                 raise PacmanAlreadyExistsException(
                     "fixed route entry", str(key))
             fixed_route_tables[key] = fixed_route_entry
+
+    @staticmethod
+    def __dijkstra_route(machine, graph, placements):
+        router = BasicDijkstraRouting()
+        return router(
+            placements=placements, machine=machine, machine_graph=graph,
+            use_progress_bar=False)
 
     def _do_fixed_routing(
             self, fixed_route_tables, board_version, placements,
@@ -248,11 +223,9 @@ class FixedRouteRouter(object):
                 # build entry and add to table and add to tables
                 fixed_route_entry = FixedRouteEntry(
                     link_ids=link_ids, processor_ids=[])
-                chip_x = (
-                    path_chip_x + ethernet_chip_x) % (machine.max_chip_x + 1)
-                chip_y = (
-                    path_chip_y + ethernet_chip_y) % (machine.max_chip_y + 1)
-                fixed_route_tables[chip_x, chip_y] = fixed_route_entry
+                chip_xy = machine.get_global_xy(
+                    path_chip_x, path_chip_y, ethernet_chip_x, ethernet_chip_y)
+                fixed_route_tables[chip_xy] = fixed_route_entry
 
         # locate where to put data
         processor_id = self._locate_destination(
