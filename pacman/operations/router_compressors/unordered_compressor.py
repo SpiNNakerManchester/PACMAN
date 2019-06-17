8b2aaed6db4a3c2c44a725533fd5a83284580567
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2019-06-17 14:22:22 +0200

    use a dict

--- pacman/operations/router_compressors/unordered_compressor.py
+++ pacman/operations/router_compressors/unordered_compressor.py
@@ -1,3 +1,7 @@
+try:
+    from collections.abc import defaultdict
+except ImportError:
+    from collections import defaultdict
 from pacman.model.routing_tables import (
     MulticastRoutingTable, MulticastRoutingTables)
 from pacman.model.routing_tables.multicast_routing_tables import (from_json, to_json)
@@ -44,22 +48,25 @@ def merge(entry1, entry2):
     new_xs = any_ones ^ any_zeros
     mask = all_selected & new_xs  # Combine existing and new Xs
     key = all_ones & mask
-    return MulticastRoutingEntry(key, mask, entry1.processor_ids, entry2.link_ids, entry1.defaultable and entry2.defaultable)
+    return key, mask
 
+def find_merge(entry1, entry2, all_entries):
+    m_key, m_mask = merge(entry1, entry2)
+    for spinnaker_route in all_entries:
+        if spinnaker_route != entry1.spinnaker_route:
+            for check in all_entries[spinnaker_route]:
+                if intersect(check.routing_entry_key, check.mask, m_key, m_mask):
+                    return None
+    return MulticastRoutingEntry(
+        m_key, m_mask, defaultable=entry1.defaultable &entry2.defaultable ,
+        spinnaker_route=entry1.spinnaker_route)
 
-def find_merge(entry1, entry2, unchecked):
-    m1 = merge(entry1, entry2)
-    for check in unchecked:
-        if intersect(m1.routing_entry_key, m1.mask, check.routing_entry_key, check.mask):
-            return None
-    return m1
-
-def compress_by_route(to_check, unchecked):
+def compress_by_route(to_check, all_entries):
     unmergable = []
     while len(to_check) > 1:
         entry = to_check.pop()
         for other in to_check:
-            merged = find_merge(entry, other, unchecked)
+            merged = find_merge(entry, other, all_entries)
             if merged is not None:
                 to_check.remove(other)
                 to_check.append(merged)
@@ -71,23 +78,24 @@ def compress_by_route(to_check, unchecked):
 
 def compress_table(router_table):
     compressed_table = MulticastRoutingTable(router_table.x, router_table.y)
-    unchecked = []
+    all_entries = defaultdict(list)
     spinnaker_routes = set()
     for entry in router_table.multicast_routing_entries:
-        unchecked.append(entry)
+        all_entries[entry.spinnaker_route].append(entry)
         spinnaker_routes.add(entry.spinnaker_route)
 
+    length = router_table.number_of_entries
     for spinnaker_route in spinnaker_routes:
-        to_check = []
-        for i in range(len(unchecked) - 1, -1, -1):
-            entry = unchecked[i]
-            if entry.spinnaker_route == spinnaker_route:
-                del unchecked[i]
-                to_check.append(entry)
-
-        for entry in compress_by_route(to_check, unchecked):
+        if length > MAX_SUPPORTED_LENGTH:
+            to_check = all_entries[spinnaker_route]
+            length -= len(to_check)
+            compressed = compress_by_route(to_check, all_entries)
+            length += len(compressed)
+            all_entries[spinnaker_route] = compressed
+        else:
+            compressed = all_entries[spinnaker_route]
+        for entry in compressed:
             compressed_table.add_multicast_routing_entry(entry)
-            unchecked.append(entry)
     print(router_table.number_of_entries, len(spinnaker_routes), compressed_table.number_of_entries)
     return compressed_table
 
@@ -96,10 +104,10 @@ def compress_tables(router_tables):
     compressed_tables = MulticastRoutingTables()
     for table in router_tables:
         print(table.number_of_entries)
-        #if table.number_of_entries < MAX_SUPPORTED_LENGTH:
-        #    compressed_table = table
-        #else:
-        compressed_table = compress_table(table)
+        if table.number_of_entries < MAX_SUPPORTED_LENGTH:
+            compressed_table = table
+        else:
+            compressed_table = compress_table(table)
         compressed_tables.add_routing_table(compressed_table)
 
     return compressed_tables
