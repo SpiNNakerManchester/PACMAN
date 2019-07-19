fd3622b6b88e41bebd60ea40d17d8dccaead1b79
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-07-19 10:16:37 +0100

    fixed merge mess

--- pacman/model/routing_tables/multicast_routing_tables.py
+++ pacman/model/routing_tables/multicast_routing_tables.py
@@ -1,10 +1,3 @@
-<<<<<<<<< Temporary merge branch 1
-try:
-    from collections.abc import OrderedDict
-except ImportError:
-    from collections import OrderedDict
-import json
-=========
 # Copyright (c) 2017-2019 The University of Manchester
 #
 # This program is free software: you can redistribute it and/or modify
@@ -20,13 +13,12 @@ import json
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import json
+import gzip
 try:
     from collections.abc import OrderedDict
 except ImportError:
     from collections import OrderedDict
-import json
-import json
-import gzip
 try:
     from collections.abc import OrderedDict
 except ImportError:
