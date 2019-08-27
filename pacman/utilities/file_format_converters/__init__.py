d62939710e2c868ec040bc3d94a20ef5c4c0bdb2
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-08-27 14:43:14 +0100

    removed unused file convertors to formtas that have been dropped!

--- pacman/utilities/file_format_converters/__init__.py
+++ pacman/utilities/file_format_converters/__init__.py
@@ -13,12 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from .convert_to_file_core_allocations import ConvertToFileCoreAllocations
-from .convert_to_file_machine import ConvertToFileMachine
-from .convert_to_file_machine_graph import ConvertToFileMachineGraph
-from .convert_to_file_machine_graph_pure_multicast import (
-    ConvertToFileMachineGraphPureMulticast)
-from .convert_to_file_placement import ConvertToFilePlacement
 from .convert_to_memory_multi_cast_routes import ConvertToMemoryMultiCastRoutes
 from .convert_to_memory_placements import ConvertToMemoryPlacements
 from .create_file_constraints import CreateConstraintsToFile
@@ -27,8 +21,5 @@ import os
 converter_algorithms_metadata_file = os.path.join(
     os.path.dirname(__file__), "converter_algorithms_metadata.xml")
 
-__all__ = ["ConvertToFileCoreAllocations", "ConvertToFileMachine",
-           "ConvertToFileMachineGraph", "ConvertToFilePlacement",
-           "ConvertToFileMachineGraphPureMulticast",
-           "ConvertToMemoryMultiCastRoutes", "ConvertToMemoryPlacements",
+__all__ = ["ConvertToMemoryMultiCastRoutes", "ConvertToMemoryPlacements",
            "CreateConstraintsToFile", "converter_algorithms_metadata_file"]
