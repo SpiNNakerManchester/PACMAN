2b126574730ac298a13f105d2b156764efb0ebed
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-08-28 14:19:16 +0100

    connective based placer is dead and needs a burial

--- pacman/operations/placer_algorithms/__init__.py
+++ pacman/operations/placer_algorithms/__init__.py
@@ -15,9 +15,8 @@
 
 from .radial_placer import RadialPlacer
 from .basic_placer import BasicPlacer
-from .connective_based_placer import ConnectiveBasedPlacer
 from .one_to_one_placer import OneToOnePlacer
 from .spreader_placer import SpreaderPlacer
 
-__all__ = ['RadialPlacer', 'BasicPlacer', 'ConnectiveBasedPlacer',
+__all__ = ['RadialPlacer', 'BasicPlacer',
            'OneToOnePlacer', "SpreaderPlacer"]
