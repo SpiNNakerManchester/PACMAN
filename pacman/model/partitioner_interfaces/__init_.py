5a8349c870be1de53745edaa3b48d9c8f07ac31b
alan-stokes <alan.barry.stokes@gmail.com>
2019-09-27 17:00:04 +0100

    trying to fix travis

---
+++ pacman/model/partitioner_interfaces/__init_.py
@@ -0,0 +1,8 @@
+from .abstract_controls_destination_of_edges import (
+    AbstractControlsDestinationOfEdges)
+from .abstract_controls_source_of_edges import AbstractControlsSourceOfEdges
+from .splitter_by_atoms import SplitterByAtoms
+from .hand_over_to_vertex import HandOverToVertex
+
+__all__ = ["AbstractControlsDestinationOfEdges", "HandOverToVertex",
+           "AbstractControlsSourceOfEdges", "SplitterByAtoms"]
