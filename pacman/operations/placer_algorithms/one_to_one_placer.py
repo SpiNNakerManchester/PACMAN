4f547e7a70437d6edfb6caa707f7585b8a2b686c
alan-stokes <alan.barry.stokes@gmail.com>
2019-10-23 16:47:29 +0100

    pep8

--- pacman/operations/placer_algorithms/one_to_one_placer.py
+++ pacman/operations/placer_algorithms/one_to_one_placer.py
@@ -12,7 +12,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from pacman.model.resources import ResourceContainer, ConstantSDRAM
 from spinn_utilities.progress_bar import ProgressBar
 from pacman.exceptions import (
     PacmanException, PacmanInvalidParameterException, PacmanValueError)
