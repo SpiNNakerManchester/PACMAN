c3ec5b2e3fe4a13aeb3f21093a9288c24817933f
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2019-11-26 08:37:22 +0000

    moved fpga and spinnaker_link vertexes

--- pacman/model/graphs/application/__init__.py
+++ pacman/model/graphs/application/__init__.py
@@ -14,13 +14,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from .application_edge import ApplicationEdge
-from .application_fpga_vertex import ApplicationFPGAVertex
 from .application_graph import ApplicationGraph
 from .application_outgoing_edge_partition import (
     ApplicationOutgoingEdgePartition)
-from .application_spinnaker_link_vertex import ApplicationSpiNNakerLinkVertex
 from .application_vertex import ApplicationVertex
 
-__all__ = ["ApplicationEdge", "ApplicationFPGAVertex", "ApplicationGraph",
-           "ApplicationOutgoingEdgePartition", "ApplicationVertex",
-           "ApplicationSpiNNakerLinkVertex"]
+__all__ = ["ApplicationEdge", "ApplicationGraph",
+           "ApplicationOutgoingEdgePartition", "ApplicationVertex"]
