c59e7ddc6fd1d3e84ef8b40c7e4087f040c5fd85
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2020-03-06 18:53:34 +0000

    Import the correct thing

--- pacman/model/resources/multi_region_sdram.py
+++ pacman/model/resources/multi_region_sdram.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from .abstract_sdram import AbstractSDRAM
-from spinn_utilities import overrides
+from spinn_utilities.overrides import overrides
 
 
 class MultiRegionSDRAM(AbstractSDRAM):
