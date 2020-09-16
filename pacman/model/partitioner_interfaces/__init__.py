1e723c1667ac6c374ffcf828206650b5768be26a
alan-stokes <alan.barry.stokes@gmail.com>
2020-09-16 15:06:38 +0100

    init natizisms

--- pacman/model/partitioner_interfaces/__init__.py
+++ pacman/model/partitioner_interfaces/__init__.py
@@ -16,10 +16,13 @@
 from .abstract_controls_destination_of_edges import (
     AbstractControlsDestinationOfEdges)
 from .abstract_controls_source_of_edges import AbstractControlsSourceOfEdges
-from .splitter_by_atoms import SplitterByAtoms
+from .legacy_partitioner_api import LegacyPartitionerAPI
 from .hand_over_to_vertex import HandOverToVertex
 from .abstract_slices_connect import AbstractSlicesConnect
+from .abstract_splitter_common import AbstractSplitterCommon
+from .abstract_splitter_partitioner import AbstractSplitterPartitioner
 
 __all__ = ["AbstractControlsDestinationOfEdges", "HandOverToVertex",
-           "AbstractControlsSourceOfEdges", "SplitterByAtoms",
-           "AbstractSlicesConnect"]
+           "AbstractControlsSourceOfEdges", "LegacyPartitionerAPI",
+           "AbstractSlicesConnect", "AbstractSplitterPartitioner",
+           "AbstractSplitterCommon"]
