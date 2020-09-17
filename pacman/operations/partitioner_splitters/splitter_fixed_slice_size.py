ddd31c0d54e5aab4437d4a4af067f8476f9b3618
alan-stokes <alan.barry.stokes@gmail.com>
2020-09-17 12:35:45 +0100

    updated to make fixed slice splitter work

--- pacman/operations/partitioner_splitters/splitter_fixed_slice_size.py
+++ pacman/operations/partitioner_splitters/splitter_fixed_slice_size.py
@@ -12,81 +12,100 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from pacman.exceptions import (
-    PacmanPartitionException, PacmanConfigurationException)
+import sys
+
+from pacman.exceptions import PacmanPartitionException
 from pacman.model.constraints.partitioner_constraints import (
     FixedVertexAtomsConstraint, MaxVertexAtomsConstraint)
 from pacman.model.graphs.common import Slice
-from pacman.model.partitioner_interfaces import AbstractSplitterCommon
-from pacman.model.partitioner_interfaces.legacy_partitioner_api import (
-    LegacyPartitionerAPI)
+from pacman.operations.partitioner_splitters.splitter_legacy import \
+    SplitterLegacy
 from pacman.utilities import utility_calls
 from spinn_utilities.overrides import overrides
 from pacman.utilities.algorithm_utilities.partition_algorithm_utilities \
     import get_remaining_constraints
 
 
-class SplitterFixedSliceSized(AbstractSplitterCommon):
+class SplitterFixedSliceSized(SplitterLegacy):
     """ Splitter that operates off the max atoms per core constraint and
     fixed atoms per core constraints. Will ensure its feasible to partition at
     that atoms per core.
     """
 
-    __slots__ = []
+    __slots__ = [
+        "__been_called",
+        "__max_atom_by_constraint",
+        "__fixed_atom_by_constraint"]
 
-    NOT_SUITABLE_VERTEX_ERROR = (
-        "The vertex {} cannot be supported by the SplitterFixedSliceSized as"
-        " the vertex does not support the required API of "
-        "LegacyPartitionerAPI. Please inherit from the class in "
-        "pacman.model.partitioner_interfaces.legacy_partitioner_api and try "
-        "again.")
+    SPLITTER_NAME = "FixedSizeSplitter"
 
-    STR_MESSAGE = "FixedSizeSplitter governing app vertex {}"
+    CONFLICT_CONSTRAINTS_ERROR_MESSAGE = (
+        "Max size of {} is incompatible with fixed size of {}")
+
+    CONFLICT_MULTIPLE_FIXED_ATOM_CONSTRAINTS = (
+        "Vertex has multiple contradictory fixed atom constraints"
+        " - cannot be both {} and {}")
 
     def __init__(self):
-        AbstractSplitterCommon.__init__(self)
+        SplitterLegacy.__init__(self, self.SPLITTER_NAME)
+        self.__been_called = False
+        self.__max_atom_by_constraint = None
+        self.__fixed_atom_by_constraint = None
 
-    def __str__(self):
-        return self.STR_MESSAGE.format(self._governed_app_vertex)
-
-    def __repr__(self):
-        return self.__str__()
-
-    @overrides(AbstractSplitterCommon.set_governed_app_vertex)
+    @overrides(SplitterLegacy.set_governed_app_vertex)
     def set_governed_app_vertex(self, app_vertex):
-        AbstractSplitterCommon.set_governed_app_vertex(self, app_vertex)
-        if not isinstance(app_vertex, LegacyPartitionerAPI):
-            raise PacmanConfigurationException(
-                self.NOT_SUITABLE_VERTEX_ERROR.format(app_vertex.label))
+        SplitterLegacy.set_governed_app_vertex(self, app_vertex)
+        self.__max_atom_by_constraint, self.__fixed_atom_by_constraint = (
+            self._compute_constraint_max_atoms())
+        if (self.__fixed_atom_by_constraint is not None and
+                self.__max_atom_by_constraint <
+                self.__fixed_atom_by_constraint):
+            raise PacmanPartitionException(
+                self.CONFLICT_CONSTRAINTS_ERROR_MESSAGE.format(
+                    self.__max_atom_by_constraint,
+                    self.__fixed_atom_by_constraint))
 
-    @overrides(AbstractSplitterCommon.create_machine_vertices)
+    @overrides(SplitterLegacy.create_machine_vertices)
     def create_machine_vertices(self, resource_tracker, machine_graph):
         atoms_per_core = self._compute_atoms_per_core(resource_tracker)
         if atoms_per_core < 1.0:
             raise PacmanPartitionException(
                 "Not enough resources available to create vertex")
         self.__split(atoms_per_core, machine_graph, resource_tracker)
+        self.__been_called = True
+        return True
 
-    @overrides(AbstractSplitterCommon.get_out_going_slices)
+    @overrides(SplitterLegacy.get_out_going_slices)
     def get_out_going_slices(self):
-        return self._governed_app_vertex.vertex_slices
+        if self.__been_called:
+            return self._governed_app_vertex.vertex_slices, True
+        else:
+            self._get_slices_estimate(), False
 
-    @overrides(AbstractSplitterCommon.get_in_coming_slices)
+    @overrides(SplitterLegacy.get_in_coming_slices)
     def get_in_coming_slices(self):
-        return self._governed_app_vertex.vertex_slices
+        if self.__been_called:
+            return self._governed_app_vertex.vertex_slices, True
+        else:
+            return self._get_slices_estimate(), False
 
-    @overrides(AbstractSplitterCommon.get_pre_vertices)
-    def get_pre_vertices(self, edge, outgoing_edge_partition):
-        return self._governed_app_vertex.machine_vertices
+    def _get_slices_estimate(self):
+        """ generates estimate of slices
 
-    @overrides(AbstractSplitterCommon.get_post_vertices)
-    def get_post_vertices(self, edge, outgoing_edge_partition,
-                          src_machine_vertex):
-        return self._governed_app_vertex.machine_vertices
+        :return: list of slices
+        """
+        slices = list()
+        atoms = (
+            int(self.__fixed_atom_by_constraint) if
+            self.__fixed_atom_by_constraint is not None else
+            int(self.__max_atom_by_constraint))
 
-    @overrides(AbstractSplitterCommon.machine_vertices_for_recording)
-    def machine_vertices_for_recording(self, variable_to_record):
-        return self._governed_app_vertex.machine_vertices
+        for first in range(0, self._governed_app_vertex.n_atoms, atoms):
+            # Determine vertex size
+            last = int(min(
+                first + atoms, self._governed_app_vertex.n_atoms) - 1)
+            slices.append(Slice(first, last))
+        return slices
 
     def __split(self, atoms_per_core, machine_graph, resource_tracker):
         """ executes the splitting of the app vertex into machine vertices by
@@ -124,6 +143,34 @@ class SplitterFixedSliceSized(AbstractSplitterCommon):
             resource_tracker.allocate_constrained_resources(
                 resources, self._governed_app_vertex.constraints)
 
+    def _compute_constraint_max_atoms(self):
+        """ searches the constraints looking for fixed and max atom
+        constraints. Will report if conflicts are in the sets.
+
+        :return: (fixed_atoms, max_atoms)
+        """
+        fixed_atoms = None
+        for fa_constraint in utility_calls.locate_constraints_of_type(
+                self._governed_app_vertex.constraints,
+                FixedVertexAtomsConstraint):
+            if fixed_atoms is not None and fixed_atoms != fa_constraint.size:
+                raise PacmanPartitionException(
+                    self.CONFLICT_MULTIPLE_FIXED_ATOM_CONSTRAINTS.format(
+                        fixed_atoms, fa_constraint.size))
+            fixed_atoms = fa_constraint.size
+
+        max_atom_values = list()
+        for max_atom_constraint in utility_calls.locate_constraints_of_type(
+                self._governed_app_vertex.constraints,
+                MaxVertexAtomsConstraint):
+            max_atom_values.append(float(max_atom_constraint.size))
+        if len(max_atom_values) != 0:
+            max_atoms = min(max_atom_values)
+        else:
+            max_atoms = sys.maxsize
+
+        return fixed_atoms, max_atoms
+
     def _compute_atoms_per_core(self, resource_tracker):
         """ Work out how many atoms per core are required for the given\
             vertex. Assumes that the first atom of the vertex is fully\
@@ -153,26 +200,11 @@ class SplitterFixedSliceSized(AbstractSplitterCommon):
         atoms_per_cpu = self._get_ratio(
             limits.cpu_cycles.get_value(), requirements.cpu_cycles.get_value())
 
-        n_atoms = None
-        for fa_constraint in utility_calls.locate_constraints_of_type(
-                self._governed_app_vertex.constraints,
-                FixedVertexAtomsConstraint):
-            if n_atoms is not None and n_atoms != fa_constraint.size:
-                raise PacmanPartitionException(
-                    "Vertex has multiple contradictory fixed atom constraints"
-                    " - cannot be both {} and {}".format(
-                        n_atoms, fa_constraint.size))
-            n_atoms = fa_constraint.size
-
-        max_atom_values = [atoms_per_sdram, atoms_per_dtcm, atoms_per_cpu]
-        for max_atom_constraint in utility_calls.locate_constraints_of_type(
-                self._governed_app_vertex.constraints,
-                MaxVertexAtomsConstraint):
-            max_atom_values.append(float(max_atom_constraint.size))
+        max_atom_values = [
+            atoms_per_sdram, atoms_per_dtcm, atoms_per_cpu,
+            self.__max_atom_by_constraint]
         max_atoms = min(max_atom_values)
 
-        if n_atoms is not None and max_atoms < n_atoms:
-            raise PacmanPartitionException(
-                "Max size of {} is incompatible with fixed size of {}".format(
-                    max_atoms, n_atoms))
-        return int(n_atoms) if n_atoms is not None else int(max_atoms)
+        return (int(self.__fixed_atom_by_constraint) if
+                self.__fixed_atom_by_constraint is not None else
+                int(max_atoms))
