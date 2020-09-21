5c246a54142fe070f143413160d184ba8d047caf
alan-stokes <alan.barry.stokes@gmail.com>
2020-09-21 13:12:55 +0100

    got splitter partitioner to be easier to read

--- pacman/operations/partition_algorithms/splitter_partitioner.py
+++ pacman/operations/partition_algorithms/splitter_partitioner.py
@@ -90,7 +90,7 @@ class SplitterPartitioner(AbstractSplitterPartitioner):
             self.__setup_objects(
                 app_graph, machine, plan_n_time_steps, pre_allocated_resources))
 
-        self._set_same_max_atoms_to_splitters(app_graph)
+        self.__set_same_max_atoms_to_splitters(app_graph)
 
         # Partition one vertex at a time
         for vertex in progress.over(vertices):
@@ -102,7 +102,7 @@ class SplitterPartitioner(AbstractSplitterPartitioner):
         # return the accepted things
         return machine_graph, resource_tracker.chips_used
 
-    def _set_same_max_atoms_to_splitters(self, app_graph):
+    def __set_same_max_atoms_to_splitters(self, app_graph):
         # Group vertices that are supposed to be the same size
         vertex_groups = get_same_size_vertex_groups(app_graph.vertices)
         for vertex in app_graph.vertices:
@@ -110,7 +110,7 @@ class SplitterPartitioner(AbstractSplitterPartitioner):
 
             # locate max atoms per core and fixed atoms per core
             possible_max_atoms = list()
-            n_atoms = None
+            fixed_n_atoms = None
             for other_vertex in partition_together_vertices:
                 possible_max_atoms.append(
                     other_vertex.get_max_atoms_per_core())
@@ -121,28 +121,31 @@ class SplitterPartitioner(AbstractSplitterPartitioner):
                 n_atom_constraints = utils.locate_constraints_of_type(
                     other_vertex.constraints, FixedVertexAtomsConstraint)
                 for constraint in n_atom_constraints:
-                    if n_atoms is not None and constraint.size != n_atoms:
+                    if (fixed_n_atoms is not None and
+                            constraint.size != fixed_n_atoms):
                         raise PacmanPartitionException(
                             self.__ERROR_MESSAGE_CONFLICT_FIXED_ATOM.format(
-                                n_atoms, constraint.size))
-                    n_atoms = constraint.size
+                                fixed_n_atoms, constraint.size))
+                    fixed_n_atoms = constraint.size
 
             max_atoms_per_core = int(min(possible_max_atoms))
-            if n_atoms is not None and max_atoms_per_core < n_atoms:
+            if (fixed_n_atoms is not None and
+                    max_atoms_per_core < fixed_n_atoms):
                 raise PacmanPartitionException(
                     self.__ERROR_MESSAGE_CONFLICT_MAX_ATOMS.format(
-                        max_atoms_per_core, n_atoms))
-            if n_atoms is not None:
-                max_atoms_per_core = n_atoms
-                if vertex.n_atoms % n_atoms != 0:
+                        max_atoms_per_core, fixed_n_atoms))
+            if fixed_n_atoms is not None:
+                max_atoms_per_core = fixed_n_atoms
+                if vertex.n_atoms % fixed_n_atoms != 0:
                     raise PacmanPartitionException(
                         self.__ERROR_MESSAGE_FAILED_DIVISION.format(
-                            vertex.n_atoms, n_atoms))
+                            vertex.n_atoms, fixed_n_atoms))
 
             for other_vertex in partition_together_vertices:
                 other_vertex.splitter_object.set_maxc_atoms_per_core(
                     max_atoms_per_core)
-            vertex.splitter_object.set_max_atoms_per_core(max_atoms_per_core)
+            vertex.splitter_object.set_max_atoms_per_core(
+                max_atoms_per_core, fixed_n_atoms is not None)
 
     def __setup_objects(
             self, app_graph, machine, plan_n_time_steps,
