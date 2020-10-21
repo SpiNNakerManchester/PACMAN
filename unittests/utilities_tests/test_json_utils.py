0fb503c535afffc977af2aab767cbd2957a953a0
alan-stokes <alan.barry.stokes@gmail.com>
2020-10-21 15:46:51 +0100

    bye bye new line

--- unittests/utilities_tests/test_json_utils.py
+++ unittests/utilities_tests/test_json_utils.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import unittest
-
 import json
 from pacman.model.constraints.key_allocator_constraints import (
     ContiguousKeyRangeContraint, FixedKeyAndMaskConstraint,
