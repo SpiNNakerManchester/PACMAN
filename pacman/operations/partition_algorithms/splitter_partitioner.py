4f3f6ad44c1d7fbf884aee3138ffafb45f5e88a0
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2020-10-29 07:21:08 +0000

    flake8

--- pacman/operations/partition_algorithms/splitter_partitioner.py
+++ pacman/operations/partition_algorithms/splitter_partitioner.py
@@ -12,7 +12,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from collections import OrderedDict
 
 from pacman.exceptions import (
     PacmanConfigurationException, PacmanPartitionException)
