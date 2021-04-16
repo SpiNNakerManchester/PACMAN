7b8c792e166873b1186c41f96b3d32d7de8f8fac
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-04-16 08:51:21 +0100

    nuked CompressibleMallocBasedRoutingInfoAllocator

--- pacman/operations/routing_info_allocator_algorithms/malloc_based_routing_allocator/__init__.py
+++ pacman/operations/routing_info_allocator_algorithms/malloc_based_routing_allocator/__init__.py
@@ -12,11 +12,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from .compressible_malloc_based_routing_info_allocator import (
-    CompressibleMallocBasedRoutingInfoAllocator)
 from .malloc_based_routing_info_allocator import (
     MallocBasedRoutingInfoAllocator)
 
 __all__ = (
-    "CompressibleMallocBasedRoutingInfoAllocator",
     "MallocBasedRoutingInfoAllocator")
