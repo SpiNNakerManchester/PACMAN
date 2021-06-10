d36db415acf9eacad000c5d7242a1e7f9bbd4c07
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2021-06-10 10:40:43 +0100

    Fix resource tracker to allow chips to be specified

--- pacman/utilities/utility_objs/resource_tracker.py
+++ pacman/utilities/utility_objs/resource_tracker.py
@@ -1091,7 +1091,6 @@ class ResourceTracker(object):
             group_ip_tags.append(this_ip_tags)
             group_reverse_ip_tags.append(this_reverse_ip_tags)
 
-        chips = None
         if x is not None and y is not None:
             chips = [(x, y)]
 
