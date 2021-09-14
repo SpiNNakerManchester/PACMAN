fc34c20f93beaf709a0e1deaca45b6dafc91916e
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-09-14 13:25:07 +0100

    bye bye PACMANAlgorithmExecutor

--- pacman/executor/__init__.py
+++ pacman/executor/__init__.py
@@ -14,6 +14,5 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from .algorithm_metadata_xml_reader import AlgorithmMetadataXmlReader
-from .pacman_algorithm_executor import PACMANAlgorithmExecutor
 
-__all__ = ["AlgorithmMetadataXmlReader", "PACMANAlgorithmExecutor"]
+__all__ = ["AlgorithmMetadataXmlReader"]
