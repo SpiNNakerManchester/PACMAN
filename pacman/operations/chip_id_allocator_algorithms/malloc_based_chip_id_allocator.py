4ce84e18c000a9ccf5ef2a7b24e90379f4b99cd2
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-11-09 10:26:35 +0000

    use init _run instead of __call__

--- pacman/operations/chip_id_allocator_algorithms/malloc_based_chip_id_allocator.py
+++ pacman/operations/chip_id_allocator_algorithms/malloc_based_chip_id_allocator.py
@@ -42,6 +42,19 @@ class NoSpiNNakerLink(PacmanConfigurationException):
                 vertex.spinnaker_link_id, vertex.board_address))
 
 
+def malloc_based_chip_id_allocator(machine, graph):
+    """
+    :param ~spinn_machine.Machine machine:
+    :param graph:
+    :type graph: Graph
+    :rtype: ~spinn_machine.Machine
+    :raises PacmanConfigurationException:
+        If a virtual chip is in an impossible position.
+    """
+    allocator = _MallocBasedChipIdAllocator()
+    return allocator._run(machine, graph)
+
+
 class _MallocBasedChipIdAllocator(ElementAllocatorAlgorithm):
     """ A Chip ID Allocation Allocator algorithm that keeps track of\
         chip IDs and attempts to allocate them as requested
@@ -58,7 +71,7 @@ class _MallocBasedChipIdAllocator(ElementAllocatorAlgorithm):
         # we only want one virtual chip per 'link'
         self._virtual_chips = dict()
 
-    def __call__(self, machine, graph=None):
+    def _run(self, machine, graph=None):
         """
         :param ~spinn_machine.Machine machine:
         :param graph:
@@ -136,16 +149,3 @@ class _MallocBasedChipIdAllocator(ElementAllocatorAlgorithm):
         chip_id = free_space_chunk.start_address
         self._allocate_elements(chip_id, 1)
         return (chip_id >> 8), (chip_id & _LOWER_16_BITS)
-
-
-def malloc_based_chip_id_allocator(machine, graph):
-    """
-    :param ~spinn_machine.Machine machine:
-    :param graph:
-    :type graph: Graph
-    :rtype: ~spinn_machine.Machine
-    :raises PacmanConfigurationException:
-        If a virtual chip is in an impossible position.
-    """
-    allocator = _MallocBasedChipIdAllocator()
-    return allocator(machine, graph)
