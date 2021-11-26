6f8c5fadbb503dfb8e22ff8c783a890f66bdcb8e
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2021-11-26 16:20:34 +0000

    Fix test for removal

--- unittests/model_tests/graph_mapper_tests/test_graph_mapping.py
+++ unittests/model_tests/graph_mapper_tests/test_graph_mapping.py
@@ -34,27 +34,6 @@ class TestGraphMapping(unittest.TestCase):
     def setUp(self):
         unittest_setup()
 
-    def test_get_edges_from_edge(self):
-        """
-        test getting the edges from a graph mapper from a edge
-        """
-        vertices = list()
-        edges = list()
-        vertices.append(SimpleMachineVertex(None, ""))
-        vertices.append(SimpleMachineVertex(None, ""))
-        edges.append(MachineEdge(vertices[0], vertices[1]))
-        edges.append(MachineEdge(vertices[1], vertices[1]))
-        sube = MachineEdge(vertices[1], vertices[0])
-        edges.append(sube)
-        edge = SimpleTestEdge(SimpleTestVertex(10, "pre"),
-                              SimpleTestVertex(5, "post"))
-        edge.remember_associated_machine_edge(sube)
-        edge.remember_associated_machine_edge(edges[0])
-        edges_from_edge = edge.machine_edges
-        self.assertIn(sube, edges_from_edge)
-        self.assertIn(edges[0], edges_from_edge)
-        self.assertNotIn(edges[1], edges_from_edge)
-
     def test_get_vertices_from_vertex(self):
         """
         test getting the vertex from a graph mapper via the vertex
@@ -99,31 +78,6 @@ class TestGraphMapping(unittest.TestCase):
         self.assertEqual(vert, vertex1.app_vertex)
         self.assertEqual(vert, vertex2.app_vertex)
         self.assertEqual([vertex1, vertex2], list(vert.machine_vertices))
-
-    def test_get_edge_from_machine_edge(self):
-        """
-        test that tests getting a edge from a graph mapper
-        """
-        vertices = list()
-        vertices.append(SimpleMachineVertex(None, ""))
-        vertices.append(SimpleMachineVertex(None, ""))
-
-        edge = SimpleTestEdge(SimpleTestVertex(10, "pre"),
-                              SimpleTestVertex(5, "post"))
-
-        edges = list()
-        edges.append(MachineEdge(vertices[0], vertices[1], app_edge=edge))
-        edges.append(MachineEdge(vertices[1], vertices[1]))
-
-        sube = MachineEdge(vertices[1], vertices[0], app_edge=edge)
-        edges.append(sube)
-
-        edge.remember_associated_machine_edge(sube)
-        edge.remember_associated_machine_edge(edges[0])
-
-        self.assertEqual(sube.app_edge, edge)
-        self.assertEqual(edges[0].app_edge, edge)
-        self.assertIsNone(edges[1].app_edge)
 
 
 if __name__ == '__main__':
