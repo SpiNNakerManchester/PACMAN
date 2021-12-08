060ab26a1a852440780ea26e987d6188b1a57d12
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2021-12-08 11:44:43 +0000

    Simplify tag allocation

--- pacman/operations/tag_allocator_algorithms/basic_tag_allocator.py
+++ pacman/operations/tag_allocator_algorithms/basic_tag_allocator.py
@@ -13,21 +13,24 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 from spinn_utilities.progress_bar import ProgressBar
 from spinn_utilities.ordered_set import OrderedSet
 from spinn_machine.tags import IPTag, ReverseIPTag
 from pacman.model.tags import Tags
-from pacman.utilities.utility_objs import ResourceTracker
+from pacman.exceptions import PacmanNotFoundError
 
 # An arbitrary range of ports from which to allocate ports to Reverse IP Tags
 _BOARD_PORTS = range(17896, 18000)
+
+# The set of tags available on any chip
+_CHIP_TAGS = range(8)
 
 
 _Task = namedtuple("_Task", "constraint, board, tag, vertex, placement")
 
 
-def basic_tag_allocator(machine, plan_n_timesteps, placements):
+def basic_tag_allocator(machine, placements):
     """
     Basic tag allocator that goes though the boards available and applies\
         the IP tags and reverse IP tags as needed.
@@ -35,123 +38,94 @@ def basic_tag_allocator(machine, plan_n_timesteps, placements):
     :param ~spinn_machine.Machine machine:
         The machine with respect to which to partition the application
         graph
-    :param int plan_n_timesteps: number of timesteps to plan for
     :param Placements placements:
     :return: list of IP Tags, list of Reverse IP Tags,
         tag allocation holder
     :rtype: tuple(list(~spinn_machine.tags.IPTag),
         list(~spinn_machine.tags.ReverseIPTag), Tags)
     """
-    resource_tracker = ResourceTracker(machine, plan_n_timesteps)
+    # Keep track of which tags are free by Ethernet chip
+    tags_available = defaultdict(lambda: OrderedSet(_CHIP_TAGS))
 
-    # Keep track of ports allocated to reverse IP tags and tags that still
-    # need a port to be allocated
-    ports_to_allocate = dict()
-    tags_to_allocate_ports = list()
+    # Keep track of which ports are free by Ethernet chip
+    ports_available = defaultdict(lambda: OrderedSet(_BOARD_PORTS))
 
-    # Check that the algorithm can handle the constraints
-    progress = ProgressBar(placements.n_placements, "Discovering tags")
-    placements_with_tags = list()
-    for placement in progress.over(placements.placements):
-        __gather_placements_with_tags(placement, placements_with_tags)
-
-    # Go through and allocate the IP tags and constrained reverse IP tags
+    # Go through placements and find tags
     tags = Tags()
-    progress = ProgressBar(placements_with_tags, "Allocating tags")
-    for placement in progress.over(placements_with_tags):
-        __allocate_tags_for_placement(
-            placement, resource_tracker, tags, ports_to_allocate,
-            tags_to_allocate_ports)
-
-    # Finally allocate ports to the unconstrained reverse IP tags
-    __allocate_ports_for_reverse_ip_tags(
-        tags_to_allocate_ports, ports_to_allocate, tags)
+    progress = ProgressBar(placements.n_placements, "Allocating tags")
+    for placement in progress.over(placements.placements):
+        resources = placement.vertex.resources_required
+        place_chip = machine.get_chip_at(placement.x, placement.y)
+        eth_chip = machine.get_chip_at(place_chip.nearest_ethernet_x,
+                                       place_chip.nearest_ethernet_y)
+        tags_on_chip = tags_available[eth_chip.x, eth_chip.y]
+        for iptag in resources.iptags:
+            alloc_chip, tag = __get_chip_and_tag(
+                iptag, eth_chip, tags_on_chip, machine, tags_available)
+            tags.add_ip_tag(
+                __create_tag(alloc_chip, placement, iptag, tag),
+                placement.vertex)
+        for reverse_iptag in resources.reverse_iptags:
+            alloc_chip, tag = __get_chip_and_tag(
+                reverse_iptag, eth_chip, tags_on_chip, machine, tags_available)
+            port = __get_port(reverse_iptag, eth_chip, ports_available)
+            tags.add_ip_tag(
+                __create_reverse_tag(
+                    eth_chip, placement, reverse_iptag, tag, port),
+                placement.vertex)
 
     return tags
 
 
-def __gather_placements_with_tags(placement, collector):
-    """
-    :param Placement placement:
-    :param list(Placement) collector:
-    """
-    requires = placement.vertex.resources_required
-    if requires.iptags or requires.reverse_iptags:
-        ResourceTracker.check_constraints([placement.vertex])
-        collector.append(placement)
-
-
-def __allocate_tags_for_placement(
-        placement, resource_tracker, tag_collector, ports_collector,
-        tag_port_tasks):
-    """
-    :param Placement placement:
-    :param ResourceTracker resource_tracker:
-    :param Tags tag_collector:
-    :param dict(str,set(int)) ports_collector:
-    :param list(_Task) tag_port_tasks:
-    """
-    vertex = placement.vertex
-    resources = vertex.resources_required
-
-    # Get the constraint details for the tags
-    (board_address, ip_tags, reverse_ip_tags) = \
-        ResourceTracker.get_ip_tag_info(resources, vertex.constraints)
-
-    # Allocate the tags, first-come, first-served, using the fixed
-    # placement of the vertex, and the required resources
-    chips = [(placement.x, placement.y)]
-    (_, _, _, returned_ip_tags, returned_reverse_ip_tags) = \
-        resource_tracker.allocate_resources(
-            resources, chips, placement.p, board_address, ip_tags,
-            reverse_ip_tags)
-
-    # Put the allocated IP tag information into the tag object
-    if returned_ip_tags is not None:
-        for (tag_constraint, (board_address, tag, dest_x, dest_y)) in \
-                zip(ip_tags, returned_ip_tags):
-            ip_tag = IPTag(
-                board_address=board_address, destination_x=dest_x,
-                destination_y=dest_y, tag=tag,
-                ip_address=tag_constraint.ip_address,
-                port=tag_constraint.port,
-                strip_sdp=tag_constraint.strip_sdp,
-                traffic_identifier=tag_constraint.traffic_identifier)
-            tag_collector.add_ip_tag(ip_tag, vertex)
-
-    if returned_reverse_ip_tags is None:
-        return
-
-    # Put the allocated reverse IP tag information into the tag object
-    for tag_constraint, (board_address, tag) in zip(
-            reverse_ip_tags, returned_reverse_ip_tags):
-        if board_address not in ports_collector:
-            ports_collector[board_address] = OrderedSet(_BOARD_PORTS)
-        if tag_constraint.port is not None:
-            reverse_ip_tag = ReverseIPTag(
-                board_address, tag, tag_constraint.port,
-                placement.x, placement.y, placement.p,
-                tag_constraint.sdp_port)
-            tag_collector.add_reverse_ip_tag(reverse_ip_tag, vertex)
-
-            ports_collector[board_address].discard(tag_constraint.port)
+def __get_chip_and_tag(iptag, eth_chip, tags_on_chip, machine, tags_available):
+    if iptag.tag is not None:
+        tag = iptag.tag
+        # Try the nearest Ethernet
+        if iptag.tag in tags_on_chip:
+            tags_on_chip.remove(iptag.tag)
+            return eth_chip, tag
         else:
-            tag_port_tasks.append(_Task(
-                tag_constraint, board_address, tag, vertex, placement))
+            return __find_tag_chip(machine, tags_available, tag), tag
+    else:
+        if tags_on_chip:
+            tag = tags_on_chip.pop()
+            return eth_chip, tag
+        else:
+            return __find_free_tag(machine, tags_available)
 
 
-def __allocate_ports_for_reverse_ip_tags(tasks, ports, tags):
-    """
-    :param list(_Task) tag_port_tasks:
-    :param dict(str,set(int)) ports:
-    :param Tags tags:
-    """
-    for task in tasks:
-        if task.board not in ports:
-            ports[task.board] = OrderedSet(_BOARD_PORTS)
-        port = ports[task.board].pop(last=False)
-        reverse_ip_tag = ReverseIPTag(
-            task.board, task.tag, port,
-            task.placement.x, task.placement.y, task.placement.p,
-            task.constraint.sdp_port)
-        tags.add_reverse_ip_tag(reverse_ip_tag, task.vertex)
+def __find_tag_chip(machine, tags_available, tag):
+    for eth_chip in machine.ethernet_connected_chips:
+        tags_on_chip = tags_available[eth_chip.x, eth_chip.y]
+        if tag in tags_on_chip:
+            tags_on_chip.remove(tag)
+            return eth_chip
+    raise PacmanNotFoundError(f"Tag {tag} not available on any Ethernet chip")
+
+
+def __find_free_tag(machine, tags_available):
+    for eth_chip in machine.ethernet_connected_chips:
+        tags_on_chip = tags_available[eth_chip.x, eth_chip.y]
+        if tags_on_chip:
+            tag = tags_on_chip.pop()
+            return eth_chip, tag
+    raise PacmanNotFoundError(f"Out of tags!")
+
+
+def __create_tag(eth_chip, placement, iptag, tag):
+    return IPTag(
+        eth_chip.ip_address, placement.x, placement.y,
+        tag, iptag.ip_address, iptag.port,
+        iptag.strip_sdp, iptag.traffic_identifier)
+
+
+def __create_reverse_tag(eth_chip, placement, reverse_iptag, tag, port):
+    return ReverseIPTag(
+        eth_chip.ip_address, tag, port, placement.x, placement.y, placement.p,
+        reverse_iptag.sdp_port)
+
+
+def __get_port(reverse_ip_tag, eth_chip, ports_available):
+    if reverse_ip_tag.port is not None:
+        return reverse_ip_tag.port
+    return ports_available[eth_chip.x, eth_chip.y].pop()
