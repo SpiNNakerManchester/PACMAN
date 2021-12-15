0e45c3f913ca60e52bcbde0af1909625fd2eba47
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-12-15 07:25:33 +0000

    added stopping stage

--- unittests/data/test_data.py
+++ unittests/data/test_data.py
@@ -69,6 +69,9 @@ class TestSimulatorData(unittest.TestCase):
             writer.runtime_graph
         with self.assertRaises(DataLocked):
             writer.runtime_machine_graph()
+        # the writer still has access to the runtime graphs
+        writer.get_runtime_graph()
+        writer.get_runtime_machine_graph()
 
         writer.start_run()
         writer.runtime_graph
@@ -89,6 +92,32 @@ class TestSimulatorData(unittest.TestCase):
             writer.runtime_graph
         with self.assertRaises(DataLocked):
             writer.runtime_machine_graph()
+        # the writer still has access to the runtime graphs
+        writer.get_runtime_graph()
+        writer.get_runtime_machine_graph()
+
+        writer.stopping()
+        writer.runtime_graph
+        writer.runtime_machine_graph
+        # the writer still has access to the user graphs
+        writer.get_graph()
+        writer.get_machine_graph()
+        # The view does not while in stopping mode
+        with self.assertRaises(DataLocked):
+            view.graph
+        with self.assertRaises(DataLocked):
+            view.machine_graph
+
+        writer.shut_down()
+        writer.graph
+        writer.machine_graph
+        with self.assertRaises(DataLocked):
+            writer.runtime_graph
+        with self.assertRaises(DataLocked):
+            writer.runtime_machine_graph()
+        # the writer still has access to the runtime graphs
+        writer.get_runtime_graph()
+        writer.get_runtime_machine_graph()
 
     def test_graph_support(self):
         view = PacmanDataView()
