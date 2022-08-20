1dfa89d0d2ec129472cc65dcca25f8ff3fe766fc
Christian <christian.brenninkmeijer@manchester.ac.uk>
2022-08-20 17:12:41 +0100

    removed unused import

--- pacman/model/partitioner_interfaces/legacy_partitioner_api.py
+++ pacman/model/partitioner_interfaces/legacy_partitioner_api.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from spinn_utilities.abstract_base import (
-    AbstractBase, abstractmethod, abstractproperty)
+    AbstractBase, abstractmethod)
 
 
 # Can't use this decorator: circular import problem
