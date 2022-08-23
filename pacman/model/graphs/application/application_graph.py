f70eb6dc8e581d57dcb41f2d92ade92b90e7ed1c
Christian <christian.brenninkmeijer@manchester.ac.uk>
2022-08-23 15:40:09 +0200

    get_edges_starting_at_vertex removed as unused

--- pacman/model/graphs/application/application_graph.py
+++ pacman/model/graphs/application/application_graph.py
@@ -161,18 +161,6 @@ class ApplicationGraph(object):
             edge_partition.pre_vertex].add(edge_partition)
         self._n_outgoing_edge_partitions += 1
 
-    def get_edges_starting_at_vertex(self, vertex):
-        """ Get all the edges that start at the given vertex.
-
-        :param AbstractVertex vertex:
-            The vertex at which the edges to get start
-        :rtype: iterable(AbstractEdge)
-        """
-        parts = self.get_outgoing_edge_partitions_starting_at_vertex(vertex)
-        for partition in parts:
-            for edge in partition.edges:
-                yield edge
-
     @property
     def outgoing_edge_partitions(self):
         """ The edge partitions in the graph.
