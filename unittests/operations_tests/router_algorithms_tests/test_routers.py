5ec1b25ce023853eccb17497d81d93f0de16b061
Christian <christian.brenninkmeijer@manchester.ac.uk>
2022-08-23 11:20:54 +0200

    super().__init__()

--- unittests/operations_tests/router_algorithms_tests/test_routers.py
+++ unittests/operations_tests/router_algorithms_tests/test_routers.py
@@ -59,7 +59,7 @@ def params(request):
 class TestSplitter(AbstractSplitterCommon):
 
     def __init__(self, n_machine_vertices):
-        AbstractSplitterCommon.__init__()
+        super().__init__()
         self.__n_machine_vertices = n_machine_vertices
 
     def create_machine_vertices(self, chip_counter):
@@ -95,7 +95,7 @@ class TestMultiInputSplitter(AbstractSplitterCommon):
     def __init__(self, n_incoming_machine_vertices,
                  n_outgoing_machine_vertices, n_groups,
                  internal_multicast=False):
-        AbstractSplitterCommon.__init__()
+        super().__init__()
         self.__n_incoming_machine_vertices = n_incoming_machine_vertices
         self.__n_outgoing_machine_vertices = n_outgoing_machine_vertices
         self.__n_groups = n_groups
@@ -182,7 +182,7 @@ class TestMultiInputSplitter(AbstractSplitterCommon):
 class TestOneToOneSplitter(AbstractSplitterCommon):
 
     def __init__(self, n_machine_vertices):
-        AbstractSplitterCommon.__init__()
+        super().__init__()
         self.__n_machine_vertices = n_machine_vertices
 
     def create_machine_vertices(self, chip_counter):
