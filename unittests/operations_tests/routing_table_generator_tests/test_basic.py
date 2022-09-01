0782217ac069ce1d7436d304996ae29555e6793b
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-09-01 10:19:55 +0100

    more table generator tests

--- unittests/operations_tests/routing_table_generator_tests/test_basic.py
+++ unittests/operations_tests/routing_table_generator_tests/test_basic.py
@@ -25,44 +25,17 @@ from pacman.operations.placer_algorithms import place_application_graph
 from pacman.operations.partition_algorithms import splitter_partitioner
 from pacman.operations.router_algorithms.application_router import (
     route_application_graph)
-from pacman.model.routing_info import RoutingInfo
 from pacman.operations.routing_info_allocator_algorithms import (
     ZonedRoutingInfoAllocator)
-from pacman.operations.routing_table_generators.merged_routing_table_generator\
-    import (merged_routing_table_generator, _IteratorWithNext)
+from pacman.operations.routing_table_generators import (
+    basic_routing_table_generator)
 from pacman_test_objects import SimpleTestVertex
 
 
-class TestMerged(unittest.TestCase):
+class TestBasic(unittest.TestCase):
 
     def setUp(self):
         unittest_setup()
-
-    def create_graphs1(self, writer):
-        v1 = SimpleTestVertex(
-            10, "app1", max_atoms_per_core=10, splitter=SplitterFixedLegacy())
-        v2 = SimpleTestVertex(
-            10, "app2", max_atoms_per_core=10, splitter=SplitterFixedLegacy())
-        writer.add_vertex(v1)
-        writer.add_vertex(v2)
-        writer.add_edge(ApplicationEdge(v1, v2), "foo")
-
-    def create_graphs2(self, writer):
-        v1 = SimpleTestVertex(
-            10, "app1", max_atoms_per_core=10, splitter=SplitterFixedLegacy())
-        v2 = SimpleTestVertex(
-            10, "app2", max_atoms_per_core=10, splitter=SplitterFixedLegacy())
-        v3 = SimpleTestVertex(
-            10, "app3", max_atoms_per_core=10, splitter=SplitterFixedLegacy())
-        v4 = SimpleTestVertex(
-            10, "app3", max_atoms_per_core=10, splitter=SplitterFixedLegacy())
-        writer.add_vertex(v1)
-        writer.add_vertex(v2)
-        writer.add_vertex(v3)
-        writer.add_vertex(v4)
-        writer.add_edge(ApplicationEdge(v1, v2), "foo")
-        writer.add_edge(ApplicationEdge(v2, v3), "foo")
-        writer.add_edge(ApplicationEdge(v3, v4), "foo")
 
     def create_graphs3(self, writer):
         v1 = SimpleTestVertex(
@@ -95,25 +68,9 @@ class TestMerged(unittest.TestCase):
     def test_empty(self):
         writer = PacmanDataWriter.mock()
         self.make_infos(writer)
-        data = merged_routing_table_generator()
+        data = basic_routing_table_generator()
         self.assertEqual(0, data.max_number_of_entries)
         self.assertEqual(0, len(list(data.routing_tables)))
-
-    def test_graph1(self):
-        writer = PacmanDataWriter.mock()
-        self.create_graphs1(writer)
-        self.make_infos(writer)
-        data = merged_routing_table_generator()
-        self.assertEqual(1, data.max_number_of_entries)
-        self.assertEqual(1, len(list(data.routing_tables)))
-
-    def test_graph2(self):
-        writer = PacmanDataWriter.mock()
-        self.create_graphs3(writer)
-        self.make_infos(writer)
-        data = merged_routing_table_generator()
-        self.assertEqual(10, data.max_number_of_entries)
-        self.assertEqual(4, len(list(data.routing_tables)))
 
     def test_graph3_with_system(self):
         writer = PacmanDataWriter.mock()
@@ -122,30 +79,6 @@ class TestMerged(unittest.TestCase):
         mv = SimpleMachineVertex(ConstantSDRAM(1))
         system_plaements.add_placement(Placement(mv, 1, 2, 3))
         self.make_infos(writer, system_plaements)
-        data = merged_routing_table_generator()
-        self.assertEqual(10, data.max_number_of_entries)
+        data = basic_routing_table_generator()
+        self.assertEqual(34, data.max_number_of_entries)
         self.assertEqual(4, len(list(data.routing_tables)))
-
-    def test_bad_infos(self):
-        writer = PacmanDataWriter.mock()
-        self.create_graphs1(writer)
-        self.make_infos(writer)
-        # overwrite the infos so it goes wrong
-        writer.set_routing_infos(RoutingInfo())
-        try:
-            merged_routing_table_generator()
-            raise Exception("Should not get here")
-        except Exception as ex:
-            self.assertIn("Missing Routing information", str(ex))
-
-    def test_iterator_with_next(self):
-        empty = _IteratorWithNext([])
-        self.assertFalse(empty.has_next)
-        with self.assertRaises(StopIteration):
-            empty.pop()
-        ten = _IteratorWithNext(range(10))
-        check = []
-        while ten.has_next:
-            # this needs to work even with a debugger look at ten here
-            check.append(ten.pop())
-        self.assertEqual(10, len(check))
