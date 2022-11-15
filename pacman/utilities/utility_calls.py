d94ed3ecef769b02b90191e37bd326234ac45d5a
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2022-11-15 09:12:15 +0000

    Add shift here too

--- pacman/utilities/utility_calls.py
+++ pacman/utilities/utility_calls.py
@@ -184,15 +184,19 @@ def get_field_based_keys(key, vertex_slice, shift=0):
     return keys + key
 
 
-def get_field_based_index(base_key, vertex_slice):
+def get_field_based_index(base_key, vertex_slice, shift=0):
     """ Map field based keys back to indices
 
     :param int base_key: The base key
     :param Slice vertex_slice: The slice to translate
+    :param int shift:
+        The left shift to apply to the atom key before adding to the key. Can
+        be used to make space for additional information at the bottom of the
+        key.
     :rtype: dict(int,int)
     """
     # Get the field based keys
-    field_based_keys = get_field_based_keys(base_key, vertex_slice)
+    field_based_keys = get_field_based_keys(base_key, vertex_slice, shift)
 
     # Inverse the index
     return {
