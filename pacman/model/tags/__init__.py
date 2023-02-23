4f694d5a95303663bd8e3d0e91055f08313bf931
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-02-23 07:17:43 +0000

    use . import for docs conf.py hack

--- pacman/model/tags/__init__.py
+++ pacman/model/tags/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pacman.model.tags.tags import Tags
+from .tags import Tags
 
 __all__ = ["Tags"]
