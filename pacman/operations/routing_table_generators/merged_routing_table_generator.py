ea7491a361fca1aaa854405b0b25ca8652b39dd0
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2023-11-03 09:57:03 +0000

    Fix typing

--- pacman/operations/routing_table_generators/merged_routing_table_generator.py
+++ pacman/operations/routing_table_generators/merged_routing_table_generator.py
@@ -20,11 +20,11 @@ from pacman.exceptions import PacmanRoutingException
 from pacman.model.routing_tables import (
     UnCompressedMulticastRoutingTable, MulticastRoutingTables)
 from pacman.model.graphs.application import ApplicationVertex
-from pacman.model.routing_info import RoutingInfo
+from pacman.model.routing_info import RoutingInfo, AppVertexRoutingInfo
 from pacman.model.graphs import AbstractVertex
 from pacman.model.routing_table_by_partition import (
     MulticastRoutingTableByPartitionEntry)
-from pacman.model.routing_info.vertex_routing_info import VertexRoutingInfo
+from pacman.model.graphs.machine.machine_vertex import MachineVertex
 
 
 def merged_routing_table_generator() -> MulticastRoutingTables:
@@ -73,20 +73,30 @@ def __create_routing_table(
         if r_info is None:
             raise PacmanRoutingException(
                 f"Missing Routing information for {vertex}, {part_id}")
-        app_r_info = None
-        if not isinstance(vertex, ApplicationVertex):
-            app_r_info = routing_info.get_routing_info_from_pre_vertex(
-                vertex.app_vertex, part_id)
+
+        # If we have an application vertex, just put the entry in and move on
+        if isinstance(vertex, ApplicationVertex):
+            table.add_multicast_routing_entry(
+                MulticastRoutingEntry(
+                    r_info.key, r_info.mask, defaultable=entry.defaultable,
+                    spinnaker_route=entry.spinnaker_route))
+            continue
+        # Otherwise it has to be a machine vertex...
+        assert isinstance(vertex, MachineVertex)
+        app_r_info = routing_info.get_routing_info_from_pre_vertex(
+            vertex.app_vertex, part_id)
+        # And this has to be AppVertexRoutingInfo!
+        assert isinstance(app_r_info, AppVertexRoutingInfo)
         entries: List[Tuple[
-            AbstractVertex, str, MulticastRoutingTableByPartitionEntry,
-            VertexRoutingInfo]] = [(vertex, part_id, entry, r_info)]
+            MulticastRoutingTableByPartitionEntry,
+            AppVertexRoutingInfo]] = [(entry, r_info)]
         while __match(iterator, vertex, part_id, r_info, entry, routing_info,
                       app_r_info):
             (vertex, part_id), entry = iterator.pop()
             r_info = routing_info.get_routing_info_from_pre_vertex(
                 vertex, part_id)
             if r_info is not None:
-                entries.append((vertex, part_id, entry, r_info))
+                entries.append((entry, r_info))
 
         # Now attempt to merge sources together as much as possible
         for mrentry in __merged_keys_and_masks(app_r_info, entries):
@@ -99,8 +109,6 @@ def __match(
         iterator, vertex, part_id, r_info, entry, routing_info,
         app_r_info) -> bool:
     if not iterator.has_next:
-        return False
-    if isinstance(vertex, ApplicationVertex):
         return False
     if r_info.mask != app_r_info.machine_mask:
         return False
@@ -138,14 +146,14 @@ def __mask_has_holes(mask: int) -> bool:
 
 
 def __merged_keys_and_masks(
-        app_r_info: VertexRoutingInfo,
+        app_r_info: AppVertexRoutingInfo,
         entries: List[Tuple[
-            AbstractVertex, str, MulticastRoutingTableByPartitionEntry,
-            VertexRoutingInfo]]) -> Iterable[MulticastRoutingEntry]:
+            MulticastRoutingTableByPartitionEntry,
+            AppVertexRoutingInfo]]) -> Iterable[MulticastRoutingEntry]:
     if not entries:
         return
-    (vertex, _, entry, r_info) = entries[0]
-    if isinstance(vertex, ApplicationVertex) or len(entries) == 1:
+    (entry, r_info) = entries[0]
+    if len(entries) == 1:
         yield MulticastRoutingEntry(
             r_info.key, r_info.mask, defaultable=entry.defaultable,
             spinnaker_route=entry.spinnaker_route)
