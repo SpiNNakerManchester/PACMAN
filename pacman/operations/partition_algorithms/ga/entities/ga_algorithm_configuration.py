db66a3a86d387b48ebbc17bafe54716d08ed26e7
Huang Wanhong <405544641@qq.com>
2024-03-16 01:17:40 +0900

    add GA algorithm configuration class

--- pacman/operations/partition_algorithms/ga/entities/ga_algorithm_configuration.py
+++ pacman/operations/partition_algorithms/ga/entities/ga_algorithm_configuration.py
@@ -1,4 +1,50 @@
+from pacman.operations.partition_algorithms.ga.solution_representations.abst_ga_solution_representation import AbstractGASolutionRepresentation
+from pacman.operations.partition_algorithms.ga.init_population_generators.abst_init_pop_generator import AbstractGaInitialPopulationGenerator
+from pacman.operations.partition_algorithms.ga.solution_representations.abst_ga_solution_representation import AbstractGASolutionRepresentation
+from pacman.operations.partition_algorithms.ga.init_population_generators.abst_init_pop_generator import AbstractGaInitialPopulationGenerator
+from pacman.operations.partition_algorithms.ga.crossover_individuals_selectors.abst_crossover_individuals_selector import AbstractGaCrossoverIndividualSelector
+from pacman.operations.partition_algorithms.ga.crossover_operators.abst_crossover import AbstractGaCrossover
+from pacman.operations.partition_algorithms.ga.variation_operators.abst_variation import AbstractGaVariation
+from pacman.operations.partition_algorithms.ga.solution_fixing_operators.abst_solution_fixing import AbstractGaSolutionFixing
+from pacman.operations.partition_algorithms.ga.cost_caculators.abst_cost_calculator import AbstractGaCostCalculator
+from pacman.operations.partition_algorithms.ga.selection_operators.abst_selection import AbstractGaSelection
+
 class GAAlgorithmConfiguration(object):
-    def __init__(self) -> None:
-        pass
-    
+    def __init__(self, init_solutions_common_representation_generator : AbstractGaInitialPopulationGenerator,
+            solution_representation_strategy: AbstractGASolutionRepresentation,
+            crossover_individuals_selection_strategy: AbstractGaCrossoverIndividualSelector, 
+            crossover_perform_strategy: AbstractGaCrossover,
+            variation_strategy: AbstractGaVariation, 
+            solution_fixing_strategy: AbstractGaSolutionFixing, 
+            solution_cost_calculation_strategy: AbstractGaCostCalculator,
+            selection_strategy: AbstractGaSelection,
+            log_processing=False,
+            output_population_all_epoch=False, 
+            output_final_epoch_population=False,
+            epochs = 10, 
+            max_individuals_each_epoch = 20,
+            remains_individuals = 10, 
+            base_path_for_output = "./") -> None:
+        self.init_solutions_common_representation_generator = init_solutions_common_representation_generator
+        self.solution_representation_strategy = solution_representation_strategy
+        self.crossover_individuals_selection_strategy = crossover_individuals_selection_strategy
+        self.crossover_perform_strategy = crossover_perform_strategy
+        self.variation_strategy = variation_strategy
+        self.solution_fixing_strategy = solution_fixing_strategy
+        self.solution_cost_calculation_strategy = solution_cost_calculation_strategy
+        self.selection_strategy = selection_strategy
+        self.log_processing = log_processing
+        self.output_population_all_epoch = output_population_all_epoch
+        self.output_final_epoch_population = output_final_epoch_population
+        self.epochs = epochs
+        self.epochs = max_individuals_each_epoch
+        self.remains_individuals = remains_individuals
+        self.base_path_for_output = base_path_for_output
+
+
+
+
+
+
+
+
