29e82efca156c4760d2593410a7d001af9c18675
Huang Wanhong <405544641@qq.com>
2024-03-18 00:38:19 +0900

    fix selection

--- pacman/operations/partition_algorithms/ga/selection_operators/possibility_selection.py
+++ pacman/operations/partition_algorithms/ga/selection_operators/possibility_selection.py
@@ -4,8 +4,9 @@ from pacman.operations.partition_algorithms.ga.selection_operators.abst_selectio
 from typing import List
 from spinn_utilities.overrides import overrides
 import random
+import numpy as np
 
-class GaEliteSelection(AbstractGaSelection):
+class GaPossibilitySelection(AbstractGaSelection):
     def __init__(self, count_survivals) -> None:
         super().__init__()
         self._count_survivals = count_survivals
@@ -13,19 +14,19 @@ class GaEliteSelection(AbstractGaSelection):
     @overrides(AbstractGaSelection._select)
     def _select(self, costs:List[float], solutions:List[AbstractGASolutionRepresentation], k_value_top_k_survival:int, count_survivals:int) -> List[AbstractGASolutionRepresentation]:
         def normalize_fitness(costs):
-            nor_costs = (costs - min(costs))/(max(costs) - min(costs))
+            if max(costs) == min(costs):
+                return [1.0 / len(costs)] ** len(costs)
+            nor_costs = (np.array(costs) - min(costs))/(max(costs) - min(costs))
             return nor_costs
-        
         select_indexes = set()
         weights = normalize_fitness(costs)
         indexes = range(0, len(solutions))
         while len(select_indexes) < count_survivals:
-            index = random.choices(indexes, weights=weights, k = 1)[0]
+            index = random.choices(indexes, weights=weights, k=1)[0]
             if(index not in select_indexes):
                 select_indexes.add(index)
         select_indexes = list(select_indexes)
-
         return [solutions[sel_index] for sel_index in select_indexes]
     
     def __str__(self):
-        return "possibility_sel"
+        return "p_sel"
