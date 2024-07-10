05ca6e5bf49f01966ab7dda03915ad8099113583
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-07-10 08:49:02 +0100

    move disable up

--- pacman/operations/placer_algorithms/_spinner_api.py
+++ pacman/operations/placer_algorithms/_spinner_api.py
@@ -19,7 +19,7 @@ from typing import (
 import sys
 from spinn_utilities.typing.coords import XY
 
-# pylint: disable=ungrouped-imports
+# pylint: disable=ungrouped-imports, no-name-in-module
 if sys.version_info.minor > 8:
     from contextlib import AbstractContextManager
 else:
