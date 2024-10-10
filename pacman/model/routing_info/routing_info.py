2d06d33b29a791a7f5f9c93b5aeae9a916fc9c60
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2024-10-10 09:49:34 +0100

    Ensure that routing information is extracted correctly (or warn)

--- pacman/model/routing_info/routing_info.py
+++ pacman/model/routing_info/routing_info.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from __future__ import annotations
-from typing import Dict, Iterator, Optional, Tuple, TYPE_CHECKING
+from collections import defaultdict
+from deprecated import deprecated
+from typing import Dict, Iterator, Optional, Iterable, TYPE_CHECKING
 from pacman.exceptions import PacmanAlreadyExistsException
 if TYPE_CHECKING:
     from .vertex_routing_info import VertexRoutingInfo
@@ -29,8 +31,8 @@ class RoutingInfo(object):
     def __init__(self) -> None:
         # Partition information indexed by edge pre-vertex and partition ID
         # name
-        self._info: Dict[
-            Tuple[AbstractVertex, str], VertexRoutingInfo] = dict()
+        self._info: Dict[AbstractVertex,
+                         Dict[str, VertexRoutingInfo]] = defaultdict(dict)
 
     def add_routing_info(self, info: VertexRoutingInfo):
         """
@@ -41,13 +43,21 @@ class RoutingInfo(object):
         :raise PacmanAlreadyExistsException:
             If the partition is already in the set of edges
         """
-        key = (info.vertex, info.partition_id)
-        if key in self._info:
+        if (info.vertex in self._info and
+                info.partition_id in self._info[info.vertex]):
             raise PacmanAlreadyExistsException(
                 "Routing information", str(info))
 
-        self._info[key] = info
+        self._info[info.vertex][info.partition_id] = info
 
+    @deprecated(reason="This method is unsafe, since it doesn't determine "
+                       "whether the info is missing because there is no "
+                       "outgoing edge, or if the outgoing edge is in another "
+                       "partition and the name is wrong. "
+                       "Use a combination of "
+                       "get_safe_routing_info_from_pre_vertex, "
+                       "get_partitions_outgoing_from_vertex, "
+                       "or get_single_routing_info_from_pre_vertex")
     def get_routing_info_from_pre_vertex(
             self, vertex: AbstractVertex,
             partition_id: str) -> Optional[VertexRoutingInfo]:
@@ -57,10 +67,34 @@ class RoutingInfo(object):
         :param AbstractVertex vertex: The vertex to search for
         :param str partition_id:
             The ID of the partition for which to get the routing information
-        :rtype: VertexRoutingInfo
+        :rtype: VertexRoutingInfo or None
         """
-        return self._info.get((vertex, partition_id))
+        return self._info[vertex].get(partition_id)
 
+    def get_safe_routing_info_from_pre_vertex(
+            self, vertex: AbstractVertex,
+            partition_id: str) -> VertexRoutingInfo:
+        """
+        Get routing information for a given partition_id from a vertex.
+
+        :param AbstractVertex vertex: The vertex to search for
+        :param str partition_id:
+            The ID of the partition for which to get the routing information
+        :rtype: VertexRoutingInfo
+        :raise KeyError:
+            If the vertex/partition_id combination is not in the routing
+            information
+        """
+        return self._info[vertex][partition_id]
+
+    @deprecated(reason="This method is unsafe, since it doesn't determine "
+                       "whether the info is missing because there is no "
+                       "outgoing edge, or if the outgoing edge is in another "
+                       "partition and the name is wrong. "
+                       "Use a combination of "
+                       "get_safe_first_key_from_pre_vertex, "
+                       "get_partitions_outgoing_from_vertex, "
+                       "or get_single_first_key_from_pre_vertex")
     def get_first_key_from_pre_vertex(
             self, vertex: AbstractVertex, partition_id: str) -> Optional[int]:
         """
@@ -70,12 +104,72 @@ class RoutingInfo(object):
         :param str partition_id:
             The ID of the partition for which to get the routing information
         :return: The routing key of the partition
-        :rtype: int
+        :rtype: int or None
         """
-        key = (vertex, partition_id)
-        if key in self._info:
-            return self._info[key].key
-        return None
+        if vertex not in self._info:
+            return None
+        info = self._info[vertex]
+        if partition_id not in info:
+            return None
+        return info[partition_id].key
+
+    def get_safe_first_key_from_pre_vertex(
+            self, vertex: AbstractVertex, partition_id: str) -> int:
+        """
+        Get the first key for the partition starting at a vertex.
+
+        :param AbstractVertex vertex: The vertex which the partition starts at
+        :param str partition_id:
+            The ID of the partition for which to get the routing information
+        :return: The routing key of the partition
+        :rtype: int
+        :raise KeyError:
+            If the vertex/partition_id combination is not in the routing
+            information
+        """
+        return self._info[vertex][partition_id].key
+
+    def get_partitions_outgoing_from_vertex(
+            self, vertex: AbstractVertex) -> Iterable[str]:
+        """
+        Get the outgoing partitions from a vertex.
+
+        :param AbstractVertex vertex: The vertex to search for
+        """
+        return self._info[vertex].keys()
+
+    def get_single_routing_info_from_pre_vertex(
+            self, vertex: AbstractVertex) -> Optional[VertexRoutingInfo]:
+        """
+        Get routing information for a given vertex.  Fails if the vertex has
+        more than one outgoing partition.
+
+        :param AbstractVertex vertex: The vertex to search for
+        :rtype: VertexRoutingInfo or None
+        :raise KeyError: If the vertex has more than one outgoing partition
+        """
+        if vertex not in self._info:
+            return None
+        info = self._info[vertex]
+        if len(info) != 1:
+            raise KeyError(
+                f"Vertex {vertex} has more than one outgoing partition")
+        return next(iter(info.values()))
+
+    def get_single_first_key_from_pre_vertex(
+            self, vertex: AbstractVertex) -> int:
+        """
+        Get the first key for the partition starting at a vertex.  Fails if
+        the vertex has more than one outgoing partition.
+
+        :param AbstractVertex vertex: The vertex which the partition starts at
+        :rtype: int
+        :raise KeyError: If the vertex has more than one outgoing partition
+        """
+        info = self.get_single_routing_info_from_pre_vertex(vertex)
+        if info is None:
+            return None
+        return info.key
 
     def __iter__(self) -> Iterator[VertexRoutingInfo]:
         """
