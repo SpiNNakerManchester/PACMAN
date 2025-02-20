6e4914f1ba66db26d5b4a65dc46c06766a85493d
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-02-20 08:07:29 +0000

    flake8

--- unittests/model_tests/routing_info_tests/test_routing_info.py
+++ unittests/model_tests/routing_info_tests/test_routing_info.py
@@ -154,7 +154,6 @@ class TestRoutingInfo(unittest.TestCase):
 
         self.assertEqual(len(routing_info), len(list(routing_info)))
 
-
     def test_multiple(self):
         routing_info = RoutingInfo()
         vertex1 = SimpleMachineVertex(ConstantSDRAM(0))
@@ -162,13 +161,11 @@ class TestRoutingInfo(unittest.TestCase):
         info = MachineVertexRoutingInfo(
             BaseKeyAndMask(key, FULL_MASK), "Test", vertex1, 0)
         routing_info.add_routing_info(info)
-        vertex2 = SimpleMachineVertex(ConstantSDRAM(0))
         key = 67890
         info = MachineVertexRoutingInfo(
             BaseKeyAndMask(key, FULL_MASK), "Test2", vertex1, 0)
         routing_info.add_routing_info(info)
         self.assertEqual(len(routing_info), len(list(routing_info)))
-
 
     def test_base_key_and_mask(self):
         with self.assertRaises(PacmanConfigurationException):
