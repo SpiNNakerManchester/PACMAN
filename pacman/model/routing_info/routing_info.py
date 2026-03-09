b6546417d705bb6cc682e26a940c8b9ed438523a
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2026-03-09 13:17:02 +0000

    global has methods

--- pacman/model/routing_info/routing_info.py
+++ pacman/model/routing_info/routing_info.py
@@ -27,8 +27,10 @@ class RoutingInfo(object):
     An association of machine vertices to a non-overlapping set of keys
     and masks.
     """
-    __slots__ = ("_info", "_min_bits_machine_and_atoms",
+    __slots__ = ("_info", "_has_fixed_keys",
+                 "_has_global_masks", "_has_shiftable_masks",
                  "_max_bits_machine", "_max_bits_atoms",
+                 "_min_bits_machine_and_atoms",
                  "_size_app_part_bits", "_size_mac_atoms_bits",
                  "_target_machine_bits", "_target_atom_bits")
 
@@ -45,6 +47,9 @@ class RoutingInfo(object):
         self._size_mac_atoms_bits = -1000
         self._target_machine_bits = -1000
         self._target_atom_bits = -1000
+        self._has_fixed_keys = False
+        self._has_shiftable_masks = True
+        self._has_global_masks = True
 
     def add_routing_info(self, info: VertexRoutingInfo) -> None:
         """
@@ -61,6 +66,13 @@ class RoutingInfo(object):
                 "Routing information", str(info))
 
         self._info[info.vertex][info.partition_id] = info
+        if info.has_fixed_keys:
+            self._has_fixed_keys = True
+        if not info.has_shiftable_masks:
+            self._has_shiftable_masks = False
+        if not info.has_global_masks:
+            self._has_global_masks = False
+
 
     @deprecated(reason="This method is unsafe, since it doesn't determine "
                        "whether the info is missing because there is no "
@@ -314,3 +326,30 @@ class RoutingInfo(object):
          and others with atoms per core.
          """
         return self._target_atom_bits
+
+    @property
+    def has_fixed_keys(self) -> bool:
+        """
+        True if ANY vertex requires fixed kleys and masks
+
+        Fixed keys may be shiftable and even global
+        """
+        return self._has_fixed_keys
+
+    @property
+    def has_shiftable_masks(self) -> bool:
+        """
+        True if all masks in ALL infos are shiftable.
+
+        Only fixed key vertices should have none shiftable masks
+        """
+        return self._has_shiftable_masks
+
+    @property
+    def has_global_masks(self) -> bool:
+        """
+        True if all masks in ALL infos are global ones defined by the zones
+
+        Global masks are always shiftable.
+        """
+        return self._has_global_masks
--- unittests/operations_tests/routing_info_algorithms_tests/test_zoned_routing_allocator.py
+++ unittests/operations_tests/routing_info_algorithms_tests/test_zoned_routing_allocator.py
@@ -310,6 +310,9 @@ def test_allocator_no_fixed() -> None:
     assert routing_info.size_mac_atoms_bits == 25
     assert routing_info.target_machine_bits == 17
     assert routing_info.target_atom_bits == 8
+    assert routing_info.has_global_masks == True
+    assert routing_info.has_shiftable_masks == True
+    assert routing_info.has_fixed_keys == False
 
 def test_fixed_only() -> None:
     unittest_setup()
@@ -324,6 +327,9 @@ def test_fixed_only() -> None:
     assert routing_info.size_mac_atoms_bits == 30
     assert routing_info.target_machine_bits == 30
     assert routing_info.target_atom_bits == 0
+    assert routing_info.has_global_masks == False
+    assert routing_info.has_shiftable_masks == False
+    assert routing_info.has_fixed_keys == True
 
 
 def test_overlap() -> None:
@@ -340,7 +346,9 @@ def test_overlap() -> None:
     assert routing_info.size_mac_atoms_bits == 30
     assert routing_info.target_machine_bits == 30
     assert routing_info.target_atom_bits == 0
-
+    assert routing_info.has_global_masks == False
+    assert routing_info.has_shiftable_masks == True
+    assert routing_info.has_fixed_keys == True
 
 def test_no_edge() -> None:
     unittest_setup()
@@ -355,6 +363,9 @@ def test_no_edge() -> None:
     assert routing_info.size_mac_atoms_bits == 32
     assert routing_info.target_machine_bits == 32
     assert routing_info.target_atom_bits == 0
+    assert routing_info.has_global_masks == True
+    assert routing_info.has_shiftable_masks == True
+    assert routing_info.has_fixed_keys == False
 
 
 def test_allocator_with_fixed() -> None:
@@ -376,6 +387,9 @@ def test_allocator_with_fixed() -> None:
     assert routing_info.size_mac_atoms_bits == 25
     assert routing_info.target_machine_bits == 17
     assert routing_info.target_atom_bits == 8
+    assert routing_info.has_global_masks == False
+    assert routing_info.has_shiftable_masks == True
+    assert routing_info.has_fixed_keys == True
 
     for partition in PacmanDataView.iterate_partitions():
         partition_id = partition.identifier
@@ -453,6 +467,9 @@ def test_big_no_fixed() -> None:
     assert routing_info.size_mac_atoms_bits == 31
     assert routing_info.target_machine_bits == 11
     assert routing_info.target_atom_bits == 20
+    assert routing_info.has_global_masks == False
+    assert routing_info.has_shiftable_masks == True
+    assert routing_info.has_fixed_keys == False
 
 
 def test_big_fixed_high() -> None:
@@ -478,6 +495,9 @@ def test_big_fixed_high() -> None:
     assert routing_info.size_mac_atoms_bits == 20
     assert routing_info.target_machine_bits == 13
     assert routing_info.target_atom_bits == 7
+    assert routing_info.has_global_masks == False
+    assert routing_info.has_shiftable_masks == False
+    assert routing_info.has_fixed_keys == True
 
 def test_big_fixed_low() -> None:
     unittest_setup()
@@ -502,6 +522,9 @@ def test_big_fixed_low() -> None:
     assert routing_info.size_mac_atoms_bits == 30
     assert routing_info.target_machine_bits == 23
     assert routing_info.target_atom_bits == 7
+    assert routing_info.has_global_masks == False
+    assert routing_info.has_shiftable_masks == True
+    assert routing_info.has_fixed_keys == True
 
 
 def test_blocked_simple() -> None:
