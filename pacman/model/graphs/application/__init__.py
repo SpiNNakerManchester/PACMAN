6b1407cc59bd9a7a31a7a2c2687e42d38971d73e
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2026-08-05 09:52:47 +0100

    change import order by using full paths for some imports

--- pacman/model/graphs/application/__init__.py
+++ pacman/model/graphs/application/__init__.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from pacman.model.graphs.application.application_vertex import (
+    ApplicationVertex,
+)
+
 from .application_2d_fpga_vertex import Application2DFPGAVertex
 from .application_2d_spinnaker_link_vertex import (
     Application2DSpiNNakerLinkVertex,
@@ -21,7 +25,6 @@ from .application_edge_partition import ApplicationEdgePartition
 from .application_fpga_vertex import ApplicationFPGAVertex
 from .application_graph import ApplicationGraph
 from .application_spinnaker_link_vertex import ApplicationSpiNNakerLinkVertex
-from .application_vertex import ApplicationVertex
 from .application_virtual_vertex import ApplicationVirtualVertex
 from .fpga_connection import FPGAConnection
 
