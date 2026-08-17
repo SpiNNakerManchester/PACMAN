07cb77622804cb0545b855ebfe643750c2592777
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2026-08-17 11:54:19 +0100

    move Never back to typing_extensions

--- unittests/operations_tests/routing_table_generator_tests/test_basic.py
+++ unittests/operations_tests/routing_table_generator_tests/test_basic.py
@@ -13,9 +13,10 @@
 # limitations under the License.
 
 import unittest
-from typing import Self, cast
+from typing import cast
 
 from parameterized import parameterized
+from typing_extensions import Self
 
 from spinn_utilities.config_holder import set_config
 
